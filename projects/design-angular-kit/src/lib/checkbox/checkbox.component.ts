238f421058cf0614187c88d33dc2dbecf5db90ea
Nicola Sanitate <n.sanitate@apuliasoft.com>
2018-06-18 17:36:45 +0200

    feat(checkbox): configura test unitari
    fix #6

--- projects/design-angular-kit/src/lib/checkbox/checkbox.component.ts
+++ projects/design-angular-kit/src/lib/checkbox/checkbox.component.ts
@@ -1,19 +1,87 @@
-import { Component, Input } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Component, Input, Output, EventEmitter, ChangeDetectorRef, forwardRef, ChangeDetectionStrategy } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
-import { ValueAccessorBase } from '../value-accessor-base';
+let identifier = 0;
+
+export class CheckboxChange {
+  source: CheckboxComponent;
+  checked: boolean;
+}
 
 @Component({
   selector: 'it-checkbox',
   templateUrl: './checkbox.component.html',
   styleUrls: ['./checkbox.component.css'],
-  providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: CheckboxComponent, multi: true }]
+  providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => CheckboxComponent), multi: true }],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CheckboxComponent extends ValueAccessorBase<boolean> {
-  @Input() label: string;
-  @Input() disabled: boolean;
+export class CheckboxComponent implements ControlValueAccessor {
+  @Input()
+  get checked(): boolean { return this._checked; }
+  set checked(value: boolean) {
+    if (value !== this.checked) {
+      this._checked = value;
+      this._changeDetectorRef.markForCheck();
+    }
+  }
+  private _checked = false;
 
-  public identifier = `checkbox-${identifier++}`;
+  @Input()
+  label: string;
+
+  @Input()
+  get disabled() { return this._disabled; }
+  set disabled(value: any) {
+    if (value !== this.disabled) {
+      this._disabled = value;
+      this._changeDetectorRef.markForCheck();
+    }
+  }
+  private _disabled = false;
+
+  @Output() readonly change: EventEmitter<CheckboxChange> =
+    new EventEmitter<CheckboxChange>();
+
+    inputId = `checkbox-${identifier++}`;
+
+  private _onTouched: () => any = () => {};
+
+  private _controlValueAccessorChangeFn: (value: any) => void = () => { };
+
+  constructor(
+    private _changeDetectorRef: ChangeDetectorRef
+  ) { }
+
+  writeValue(value: any) {
+    this.checked = !!value;
+  }
+
+  registerOnChange(fn: (value: any) => void) {
+    this._controlValueAccessorChangeFn = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this._onTouched = fn;
+  }
+
+  handleChange(event: Event) {
+    event.stopPropagation();
+    if (!this.disabled) {
+      this._toggle();
+      this._emitChangeEvent();
+    }
+  }
+
+  private _toggle(): void {
+    this.checked = !this.checked;
+  }
+
+  private _emitChangeEvent() {
+    const event = new CheckboxChange();
+    event.source = this;
+    event.checked = this.checked;
+
+    this._controlValueAccessorChangeFn(this.checked);
+    this.change.emit(event);
+  }
 }
-
-let identifier = 0;
