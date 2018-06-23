4520f510f9d4997d3531cb033412f174e9b6e852
Nicola Sanitate <n.sanitate@apuliasoft.com>
2018-06-23 20:31:23 +0200

    test(checkbox): rifattorizza le variabili della classe di supporto

--- e2e/src/checkbox/checkbox.po.ts
+++ e2e/src/checkbox/checkbox.po.ts
@@ -1,13 +1,18 @@
 import { browser, by, element } from 'protractor';
 
 export class CheckboxPage {
-  private readonly ID_CHECBOX_SPUNTATO = 'checkbox-0';
-  private readonly ID_CHECBOX_DISABILITATO = 'checkbox-1';
-  private readonly ID_CHECBOX_RISULTATO = 'checkbox-2';
+  private readonly ID_CHECKBOX_SPUNTATO = 'checkbox-0';
+  private readonly ID_CHECKBOX_DISABILITATO = 'checkbox-1';
+  private readonly ID_CHECKBOX_RISULTATO = 'checkbox-2';
 
-  private readonly ATTR_FOR_LABEL_SPUNTATO = this.getLabelForAttribute(this.ID_CHECBOX_SPUNTATO);
-  private readonly ATTR_FOR_LABEL_DISABILITATO = this.getLabelForAttribute(this.ID_CHECBOX_DISABILITATO);
-  private readonly ATTR_FOR_LABEL_RISULTATO = this.getLabelForAttribute(this.ID_CHECBOX_RISULTATO);
+  // [for="checkbox-0"]
+  private readonly CSS_SELECTOR_LABEL_SPUNTATO = this.getLabelForAttribute(this.ID_CHECKBOX_SPUNTATO);
+
+  // [for="checkbox-1"]
+  private readonly CSS_SELECTOR_LABEL_DISABILITATO = this.getLabelForAttribute(this.ID_CHECKBOX_DISABILITATO);
+
+  // [for="checkbox-2"]
+  private readonly CSS_SELECTOR_LABEL_RISULTATO = this.getLabelForAttribute(this.ID_CHECKBOX_RISULTATO);
 
   private readonly ATTR_CHECKED = 'checked';
   private readonly ATTR_DISABLED = 'disabled';
@@ -17,27 +22,27 @@ export class CheckboxPage {
   }
 
   async clickSpuntatoCheckbox() {
-    await element(by.css(this.ATTR_FOR_LABEL_SPUNTATO)).click();
+    await element(by.css(this.CSS_SELECTOR_LABEL_SPUNTATO)).click();
   }
 
   async clickDisabilitatoCheckbox() {
-    await element(by.css(this.ATTR_FOR_LABEL_DISABILITATO)).click();
+    await element(by.css(this.CSS_SELECTOR_LABEL_DISABILITATO)).click();
   }
 
   async clickRisultatoCheckbox() {
-    await element(by.css(this.ATTR_FOR_LABEL_RISULTATO)).click();
+    await element(by.css(this.CSS_SELECTOR_LABEL_RISULTATO)).click();
   }
 
   async getRisultatoCheckboxChecked() {
-    return this.getRisultatoCheckbox().getAttribute(this.ATTR_CHECKED);
+    return await this.getRisultatoCheckbox().getAttribute(this.ATTR_CHECKED);
   }
 
   async getRisultatoCheckboxDisabled() {
-    return this.getRisultatoCheckbox().getAttribute(this.ATTR_DISABLED);
+    return await this.getRisultatoCheckbox().getAttribute(this.ATTR_DISABLED);
   }
 
   private getRisultatoCheckbox() {
-    return element(by.id(this.ID_CHECBOX_RISULTATO));
+    return element(by.id(this.ID_CHECKBOX_RISULTATO));
   }
 
   private getLabelForAttribute(attr: string) {
