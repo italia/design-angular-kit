bc3e93e7ab5179207d0fd4496fb449dd4626a847
Francesco Pavone <ciccio1986@gmail.com>
2018-07-11 13:08:15 +0200

    test script

--- github-pages.sh
+++ github-pages.sh
@@ -5,18 +5,12 @@ git clone git@github.com:$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git -
 
 rm -rfv docs/*
 cp -r design-angular-kit/dist/design-angular-kit-bundle/* docs
-pwd
-ls -l docs
 
-# cd examples/todomvc
-# npm install
-# npm run build
-# cd ../..
-# mkdir -p public/todomvc
-# cp examples/todomvc/public/* public/todomvc
-# cd public
-# git config --global user.email "oyanglulu@gmail.com"
-# git config --global user.name $CIRCLE_PROJECT_USERNAME
-# git add .
-# git commit -m "publi:ship: $CIRCLE_BUILD_NUM"
-# git push origin gh-pages
+cd docs
+
+git config --global user.email "teamdigitale@governo.it"
+git config --global user.name $CIRCLE_PROJECT_USERNAME
+
+git add .
+git commit -m "publi:ship: $CIRCLE_BUILD_NUM"
+git push --force origin gh-pages
