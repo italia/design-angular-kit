b7b8360304b6067662ed4ce21bce36f80192d35a
Francesco Pavone <ciccio1986@gmail.com>
2018-07-11 17:40:16 +0200

    test script

--- github-pages.sh
+++ github-pages.sh
@@ -8,9 +8,6 @@ cp -r design-angular-kit/dist/design-angular-kit-bundle/* docs
 
 cd docs
 
-git config --global user.email "teamdigitale@governo.it"
-git config --global user.name $CIRCLE_PROJECT_USERNAME
-
 git add -A
 git commit -m "Update GitHub Pages $CIRCLE_BUILD_NUM  [ci skip]"
 git push --force origin gh-pages
