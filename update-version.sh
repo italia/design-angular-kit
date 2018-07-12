14267d5dd7d31610e22002088c5e806c410360d5
Nicola Sanitate <n.sanitate@apuliasoft.com>
2018-07-12 17:30:58 +0200

    chore: aggiorna la configurazione di semantic release
    fix #26

--- update-version.sh
+++ update-version.sh
@@ -2,4 +2,5 @@
 PACKAGE_VERSION=$(node -pe "require('./dist/design-angular-kit/package.json').version")
 
 sed -i -E -e "s/\"version\": \"[0-9]+\.[0-9]+\.[0-9]+\"/\"version\": \"${PACKAGE_VERSION}\"/g" ./package.json
+sed -i -E -e "s/\"version\": \"[0-9]+\.[0-9]+\.[0-9]+\"/\"version\": \"${PACKAGE_VERSION}\"/g" ./package-lock.json
 sed -i -E -e "s/\"version\": \"[0-9]+\.[0-9]+\.[0-9]+\"/\"version\": \"${PACKAGE_VERSION}\"/g" ./projects/design-angular-kit/package.json
