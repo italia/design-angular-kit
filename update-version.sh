ccf8404f34e01bcb9aaf9c22bd686ca5b752fc30
Nicola Sanitate <n.sanitate@apuliasoft.com>
2018-07-12 16:39:17 +0200

    Test semantic release

--- update-version.sh
+++ update-version.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 PACKAGE_VERSION=$(node -pe "require('./dist/design-angular-kit/package.json').version")
 
-sed -i '' -E -e "s/\"version\": \"[0-9]+\.[0-9]+\.[0-9]+\"/\"version\": \"${PACKAGE_VERSION}\"/g" ./package.json
-sed -i '' -E -e "s/\"version\": \"[0-9]+\.[0-9]+\.[0-9]+\"/\"version\": \"${PACKAGE_VERSION}\"/g" ./projects/design-angular-kit/package.json
+sed -i -E -e "s/\"version\": \"[0-9]+\.[0-9]+\.[0-9]+\"/\"version\": \"${PACKAGE_VERSION}\"/g" ./package.json
+sed -i -E -e "s/\"version\": \"[0-9]+\.[0-9]+\.[0-9]+\"/\"version\": \"${PACKAGE_VERSION}\"/g" ./projects/design-angular-kit/package.json
