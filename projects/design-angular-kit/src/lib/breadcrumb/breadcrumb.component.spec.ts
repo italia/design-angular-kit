e88f5215cd27a08e16542e573dc3c26226c406c2
Mario Traetta <mario.traetta@apuliasoft.com>
2018-08-07 16:24:13 +0200

    test(Breadcrumb): Avvia l'implementazione dei test di unità per il componente Breadcrumb
    ref #46

--- projects/design-angular-kit/src/lib/breadcrumb/breadcrumb.component.spec.ts
+++ projects/design-angular-kit/src/lib/breadcrumb/breadcrumb.component.spec.ts
@@ -1,25 +1,114 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BreadcrumbComponent } from './breadcrumb.component';
+import { Component, DebugElement } from '@angular/core';
+import { BreadcrumbItemComponent } from './breadcrumb-item.component';
+import { By } from '@angular/platform-browser';
+
+const URL = 'http://www.google.com';
+const ICON = 'it-favorite';
+const LABEL = 'Label';
+
+
+/** Componente per testare un BreadcrumbComponent. */
+@Component({
+  template: `
+  <it-breadcrumb [dark]="isDark" [separator]="separator">
+    <it-breadcrumb-item *ngFor="let item of items"
+      [link]="item.link" [icon]="item.icon">
+      {{item.label}}
+    </it-breadcrumb-item>
+  </it-breadcrumb>
+  `,
+})
+class SingleBreadcrumbComponent {
+  dark = false;
+  separator = '/';
+  items = [
+    { link: URL, icon: ICON, label: LABEL },
+    { link: URL, icon: ICON, label: LABEL },
+    { link: URL, icon: ICON, label: LABEL },
+  ];
+}
 
 describe('BreadcrumbComponent', () => {
-  let component: BreadcrumbComponent;
-  let fixture: ComponentFixture<BreadcrumbComponent>;
+  let fixture: ComponentFixture<SingleBreadcrumbComponent>;
+  let testComponent: SingleBreadcrumbComponent;
+  let breadcrumbDebugElement: DebugElement;
+  let breadcrumbItemDebugElements: DebugElement[];
+  let breadcrumbInstance: BreadcrumbComponent;
+  let breadcrumbItemInstances: BreadcrumbItemComponent[];
+  let breadcrumbItemLabelElements: HTMLLabelElement[];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BreadcrumbComponent ]
+      declarations: [
+        BreadcrumbComponent,
+        BreadcrumbItemComponent,
+        SingleBreadcrumbComponent
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(BreadcrumbComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(SingleBreadcrumbComponent);
     fixture.detectChanges();
+
+    testComponent = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
+
+    breadcrumbDebugElement = fixture.debugElement.query(By.directive(BreadcrumbComponent));
+    breadcrumbInstance = breadcrumbDebugElement.injector.get<BreadcrumbComponent>(BreadcrumbComponent);
+
+    breadcrumbItemDebugElements = fixture.debugElement.queryAll(By.directive(BreadcrumbItemComponent));
+    breadcrumbItemInstances = breadcrumbItemDebugElements.map(debugEl => debugEl.componentInstance);
+
+    breadcrumbItemLabelElements = breadcrumbItemDebugElements.map(
+      debugEl => debugEl.query(By.css('a')).nativeElement
+    );
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('dovrebbe avere un ultimo elemento attivo', () => {
+    expect(breadcrumbItemInstances.pop().isLast).toBeTruthy();
   });
+
+  it('dovrebbe poter rilevare cambi di elementi a runtime', () => {
+    const lastItem = breadcrumbItemInstances[breadcrumbItemInstances.length - 1];
+    expect(lastItem.isLast).toBeTruthy();
+
+    testComponent.items.push({
+      icon: '', link: '', label: ''
+    });
+    fixture.detectChanges();
+
+    breadcrumbItemDebugElements = fixture.debugElement.queryAll(By.directive(BreadcrumbItemComponent));
+    breadcrumbItemInstances = breadcrumbItemDebugElements.map(debugEl => debugEl.componentInstance);
+    breadcrumbItemLabelElements = breadcrumbItemDebugElements.map(
+      debugEl => debugEl.query(By.css('a')).nativeElement
+    );
+
+    expect(lastItem.isLast).toBeFalsy();
+
+    const newLastItem = breadcrumbItemInstances[breadcrumbItemInstances.length - 1];
+    expect(newLastItem.isLast).toBeTruthy();
+
+    testComponent.items.pop();
+    fixture.detectChanges();
+
+    expect(lastItem.isLast).toBeTruthy();
+  });
+
+  it('dovrebbe poter cambiare il separatore', () => {
+    testComponent.separator = '~';
+    fixture.detectChanges();
+
+    expect(breadcrumbInstance.separator).toBe('~');
+    let isSeparatorSetforItems = true;
+    breadcrumbItemInstances.forEach(
+      item => isSeparatorSetforItems = (isSeparatorSetforItems && item.separator === '~')
+    );
+    expect(isSeparatorSetforItems).toBeTruthy();
+  });
+
 });
