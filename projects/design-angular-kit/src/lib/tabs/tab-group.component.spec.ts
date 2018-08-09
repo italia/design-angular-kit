76ab388a07a6287fea8311d714c12f4a11831ee8
Francesco Pavone <ciccio1986@gmail.com>
2018-08-09 16:12:45 +0200

    rimuovi test inutili

--- projects/design-angular-kit/src/lib/tabs/tab-group.component.spec.ts
+++ projects/design-angular-kit/src/lib/tabs/tab-group.component.spec.ts
@@ -3,23 +3,5 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TabGroupComponent } from './tab-group.component';
 
 describe('TabGroupComponent', () => {
-  let component: TabGroupComponent;
-  let fixture: ComponentFixture<TabGroupComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TabGroupComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TabGroupComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
