5c4e2295d0a61c24196fbb1304dc418bf36f960d
Mario Traetta <mario.traetta@apuliasoft.com>
2018-08-20 15:08:38 +0200

    test(Popover): Riadatta i test e2e del popover in base al nuovo esempio interattivo nella documentaz
    ref #23

--- e2e/src/popover/popover.e2e-spec.ts
+++ e2e/src/popover/popover.e2e-spec.ts
@@ -23,70 +23,84 @@ describe('Popover', () => {
   });
 
   it('dovrebbe iniziare creato e visualizzato in alto', async () => {
-    expect(await page.existsTopExamplePopover()).toBeFalsy();
-    await page.clickOverTopExample();
-    expect(await page.existsTopExamplePopover()).toBeTruthy();
-    expect(await page.getTopExamplePopoverClass()).toBe('popover bs-popover-top');
+    expect(await page.existsStandardExamplePopover()).toBeFalsy();
+
+    await page.clickOverTopPlacement();
+    await page.clickOverStandardExample();
+
+    expect(await page.existsStandardExamplePopover()).toBeTruthy();
+    expect(await page.getPlacementExamplePopoverClass()).toBe('popover bs-popover-top');
   });
 
   it('dovrebbe iniziare creato e visualizzato a destra', async () => {
-    expect(await page.existsRightExamplePopover()).toBeFalsy();
-    await page.clickOverRightExample();
-    expect(await page.existsRightExamplePopover()).toBeTruthy();
-    expect(await page.getRightExamplePopoverClass()).toBe('popover bs-popover-right');
+    expect(await page.existsStandardExamplePopover()).toBeFalsy();
+
+    await page.clickOverRightPlacement();
+    await page.clickOverStandardExample();
+
+    expect(await page.existsStandardExamplePopover()).toBeTruthy();
+    expect(await page.getPlacementExamplePopoverClass()).toBe('popover bs-popover-right');
   });
 
   it('dovrebbe iniziare creato e visualizzato a sinistra', async () => {
-    expect(await page.existsLeftExamplePopover()).toBeFalsy();
-    await page.clickOverLeftExample();
-    expect(await page.existsLeftExamplePopover()).toBeTruthy();
-    expect(await page.getLeftExamplePopoverClass()).toBe('popover bs-popover-left');
+    expect(await page.existsStandardExamplePopover()).toBeFalsy();
+
+    await page.clickOverLeftPlacement();
+    await page.clickOverStandardExample();
+
+    expect(await page.existsStandardExamplePopover()).toBeTruthy();
+    expect(await page.getPlacementExamplePopoverClass()).toBe('popover bs-popover-left');
   });
 
   it('dovrebbe iniziare creato e visualizzato in basso', async () => {
-    expect(await page.existsBottomExamplePopover()).toBeFalsy();
-    await page.clickOverBottomExample();
-    expect(await page.existsBottomExamplePopover()).toBeTruthy();
-    expect(await page.getBottomExamplePopoverClass()).toBe('popover bs-popover-bottom');
+    expect(await page.existsStandardExamplePopover()).toBeFalsy();
+
+    await page.clickOverBottomPlacement();
+    await page.clickOverStandardExample();
+
+    expect(await page.existsStandardExamplePopover()).toBeTruthy();
+    expect(await page.getPlacementExamplePopoverClass()).toBe('popover bs-popover-bottom');
   });
 
   it('dovrebbe iniziare creato e visualizzato a destra, poi in alto e successivamente a sinistra, ed infine in basso', async () => {
-    await page.clickOverPlacementExample();
+    await page.clickOverStandardExample();
     expect(await page.existsPlacementExamplePopover()).toBeTruthy();
     expect(await page.getPlacementExamplePopoverClass()).toBe('popover bs-popover-right');
 
-    await page.changePopoverPlacement();
+    await page.clickOverTopPlacement();
     expect(await page.getPlacementExamplePopoverClass()).toBe('popover bs-popover-top');
 
-    await page.changePopoverPlacement();
+    await page.clickOverLeftPlacement();
     expect(await page.getPlacementExamplePopoverClass()).toBe('popover bs-popover-left');
 
-    await page.changePopoverPlacement();
+    await page.clickOverBottomPlacement();
     expect(await page.getPlacementExamplePopoverClass()).toBe('popover bs-popover-bottom');
   });
 
   it('dovrebbe iniziare creato e visualizzato, per poi essere chiuso con un click successivo', async () => {
-    await page.clickOverFocusExample();
+    await page.clickOverFocusTrigger();
+
+    await page.clickOverStandardExample();
     const isPresent = await page.existsFocusExamplePopover();
     expect(isPresent).toBeTruthy();
 
-    await page.hideFocusExamplePopover();
+    await page.clickOverFocusTrigger();
 
-    const isHidden = await page.hiddenFocusExamplePopover();
+    const isHidden = await page.getFocusExamplePopoverClass();
     expect(isHidden).toBeTruthy();
   });
 
   it('dovrebbe iniziare creato e visualizzato, per poi essere distrutto ed infine ricreato e rivisualizzato', async () => {
-    await page.clickOverDisposableExample();
+    await page.clickOverStandardExample();
     const isPresent = await page.existsDisposableExamplePopover();
     expect(isPresent).toBeTruthy();
 
-    await page.disposeExamplePopover();
+    await page.clickOverDisposeButton();
 
     const isDisposed = await page.existsDisposableExamplePopover();
     expect(isDisposed).toBeFalsy();
 
-    await page.clickOverDisposableExample();
+    await page.clickOverStandardExample();
     expect(await page.existsDisposableExamplePopover()).toBeTruthy();
   });
 
--- e2e/src/popover/popover.po.ts
+++ e2e/src/popover/popover.po.ts
@@ -1,159 +1,100 @@
 import { browser, by, element, ExpectedConditions, ElementFinder } from 'protractor';
 
 export class PopoverPage {
-  private readonly TOOLTIP_URL = '/#/componenti/popover';
+  private readonly POPOVER_URL = '/#/componenti/popover';
   private readonly ID_EXAMPLE_TAB = 'popover-examples-tab';
 
-  private readonly ID_TOOLTIP_PLACEMENT_BUTTON = 'popover-placements-button';
-  private readonly ID_TOOLTIP_DISPOSE_BUTTON = 'popover-dispose-button';
-  private readonly ID_TOOLTIP_FOCUS_BUTTON  = 'popover-focus-button';
+  private readonly ID_POPOVER_INTERACTIVE_BUTTON = 'popover-interactive-button';
+  private readonly ID_POPOVER_DISPOSE_BUTTON = 'popover-disposing-button';
 
-  private readonly ID_EXAMPLE_STANDARD = 'popover-example-simple';
-  private readonly ID_EXAMPLE_TOP = 'popover-example-top';
-  private readonly ID_EXAMPLE_RIGHT = 'popover-example-right';
-  private readonly ID_EXAMPLE_LEFT = 'popover-example-left';
-  private readonly ID_EXAMPLE_BOTTOM = 'popover-example-bottom';
-  private readonly ID_EXAMPLE_PLACEMENT = 'popover-example-placements';
-  private readonly ID_EXAMPLE_FOCUS = 'popover-example-focus';
-  private readonly ID_EXAMPLE_DISPOSABLE = 'popover-example-disposable';
+  private readonly ID_CHECKBOX_DISABLED = this.getLabelForAttribute('checkbox-0');
+  private readonly ID_CHECKBOX_TITLE = this.getLabelForAttribute('checkbox-1');
 
-  private readonly ID_TOOLTIP_STANDARD = 'it-popover-0';
-  private readonly ID_TOOLTIP_TOP = 'it-popover-2';
-  private readonly ID_TOOLTIP_RIGHT = 'it-popover-3';
-  private readonly ID_TOOLTIP_LEFT = 'it-popover-4';
-  private readonly ID_TOOLTIP_BOTTOM = 'it-popover-5';
-  private readonly ID_TOOLTIP_PLACEMENT = 'it-popover-6';
-  private readonly ID_TOOLTIP_FOCUS = 'it-popover-7';
-  private readonly ID_TOOLTIP_DISPOSABLE = 'it-popover-10';
+  private readonly ID_RADIO_ADHOC = this.getLabelForAttribute('radio-1');
+  private readonly ID_RADIO_BODY = this.getLabelForAttribute('radio-2');
+
+  private readonly ID_RADIO_CLICK = this.getLabelForAttribute('radio-4');
+  private readonly ID_RADIO_FOCUS = this.getLabelForAttribute('radio-5');
+
+  private readonly ID_RADIO_RIGHT = this.getLabelForAttribute('radio-7');
+  private readonly ID_RADIO_TOP = this.getLabelForAttribute('radio-8');
+  private readonly ID_RADIO_LEFT = this.getLabelForAttribute('radio-9');
+  private readonly ID_RADIO_BOTTOM = this.getLabelForAttribute('radio-10');
+
+  private readonly ID_POPOVER_STANDARD = 'it-popover-0';
 
   async go() {
-    await browser.get(this.TOOLTIP_URL);
+    await browser.get(this.POPOVER_URL);
     await element(by.id(this.ID_EXAMPLE_TAB)).click();
     return await browser.sleep(500);
   }
 
   private getLabelForAttribute(attr: string) {
-    return `[id="${attr}"]`;
+    return `[for="${attr}"]`;
   }
 
   async click(elementId: string) {
-    await element(by.id(elementId)).click();
+    await element(by.css(elementId)).click();
   }
 
   async clickOverStandardExample() {
-    await this.click(this.ID_EXAMPLE_STANDARD);
+    await element(by.id(this.ID_POPOVER_INTERACTIVE_BUTTON)).click();
   }
 
-  async clickOverTopExample() {
-    await this.click(this.ID_EXAMPLE_TOP);
+  async clickOverTopPlacement() {
+    await this.click(this.ID_RADIO_TOP);
   }
 
-  async clickOverRightExample() {
-    await this.click(this.ID_EXAMPLE_RIGHT);
+  async clickOverRightPlacement() {
+    await this.click(this.ID_RADIO_RIGHT);
   }
 
-  async clickOverBottomExample() {
-    await this.click(this.ID_EXAMPLE_BOTTOM);
+  async clickOverBottomPlacement() {
+    await this.click(this.ID_RADIO_BOTTOM);
   }
 
-  async clickOverLeftExample() {
-    await this.click(this.ID_EXAMPLE_LEFT);
+  async clickOverLeftPlacement() {
+    await this.click(this.ID_RADIO_LEFT);
   }
 
   async existsStandardExamplePopover() {
-    return await element(by.id(this.ID_TOOLTIP_STANDARD)).isPresent();
+    return await element(by.id(this.ID_POPOVER_STANDARD)).isPresent();
   }
 
   async getStandardExamplePopoverClass() {
-    return await element(by.id(this.ID_TOOLTIP_STANDARD)).getAttribute('class');
-  }
-
-  async existsTopExamplePopover() {
-    return await element(by.id(this.ID_TOOLTIP_TOP)).isPresent();
-  }
-
-  async getTopExamplePopoverClass() {
-    return await element(by.id(this.ID_TOOLTIP_TOP)).getAttribute('class');
-  }
-
-  async existsRightExamplePopover() {
-    return await element(by.id(this.ID_TOOLTIP_RIGHT)).isPresent();
-  }
-
-  async getRightExamplePopoverClass() {
-    return await element(by.id(this.ID_TOOLTIP_RIGHT)).getAttribute('class');
-  }
-
-  async existsBottomExamplePopover() {
-    return await element(by.id(this.ID_TOOLTIP_BOTTOM)).isPresent();
-  }
-
-  async getBottomExamplePopoverClass() {
-    return await element(by.id(this.ID_TOOLTIP_BOTTOM)).getAttribute('class');
-  }
-
-  async existsLeftExamplePopover() {
-    return await element(by.id(this.ID_TOOLTIP_LEFT)).isPresent();
-  }
-
-  async getLeftExamplePopoverClass() {
-    return await element(by.id(this.ID_TOOLTIP_LEFT)).getAttribute('class');
-  }
-
-  async clickOverPlacementExample() {
-    await this.click(this.ID_EXAMPLE_PLACEMENT);
+    return await element(by.id(this.ID_POPOVER_STANDARD)).getAttribute('class');
   }
 
   async existsPlacementExamplePopover() {
-    return await element(by.id(this.ID_TOOLTIP_PLACEMENT)).isPresent();
+    return await element(by.id(this.ID_POPOVER_STANDARD)).isPresent();
   }
 
   async getPlacementExamplePopoverClass() {
-    return await element(by.id(this.ID_TOOLTIP_PLACEMENT)).getAttribute('class');
-  }
-
-  async changePopoverPlacement() {
-    await element(by.id(this.ID_TOOLTIP_PLACEMENT_BUTTON)).click();
-  }
-
-  async clickOverDisposableExample() {
-    await this.click(this.ID_EXAMPLE_DISPOSABLE);
+    return await element(by.id(this.ID_POPOVER_STANDARD)).getAttribute('class');
   }
 
   async clickOverDisposeButton() {
-    await this.click(this.ID_TOOLTIP_DISPOSE_BUTTON);
+    await element(by.id(this.ID_POPOVER_DISPOSE_BUTTON)).click();
   }
 
   async existsDisposableExamplePopover() {
-    return await element(by.id(this.ID_TOOLTIP_DISPOSABLE)).isPresent();
+    return await element(by.id(this.ID_POPOVER_STANDARD)).isPresent();
   }
 
   async getDisposableExamplePopoverClass() {
-    return await element(by.id(this.ID_TOOLTIP_DISPOSABLE)).getAttribute('class');
+    return await element(by.id(this.ID_POPOVER_STANDARD)).getAttribute('class');
   }
 
-  async disposeExamplePopover() {
-    await element(by.id(this.ID_TOOLTIP_DISPOSE_BUTTON)).click();
-  }
-
-  async clickOverFocusExample() {
-    await this.click(this.ID_EXAMPLE_FOCUS);
+  async clickOverFocusTrigger() {
+    await this.click(this.ID_RADIO_FOCUS);
   }
 
   async existsFocusExamplePopover() {
-    return await element(by.id(this.ID_TOOLTIP_FOCUS)).isPresent();
+    return await element(by.id(this.ID_POPOVER_STANDARD)).isPresent();
   }
 
   async getFocusExamplePopoverClass() {
-    return await element(by.id(this.ID_TOOLTIP_FOCUS)).getAttribute('class');
-  }
-
-  async hideFocusExamplePopover() {
-    await this.click('popover-lose-focus');
-  }
-
-  async hiddenFocusExamplePopover() {
-    return await element(by.id(this.ID_TOOLTIP_FOCUS)).getAttribute('hidden');
+    return await element(by.id(this.ID_POPOVER_STANDARD)).getAttribute('hidden');
   }
 
 }
