c4d118a579a4aaf37981f12956be7c98256dca74
Francesco Pavone <ciccio1986@gmail.com>
2018-08-22 11:41:01 +0200

    refactor: crea componente per mostrare la documentazione dei parametri di input/output dei component
    fix #40

---
+++ src/app/shared/api-parameters/api-parameters.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'it-api-parameters',
+  templateUrl: './api-parameters.component.html',
+  styleUrls: ['./api-parameters.component.scss']
+})
+export class ApiParametersComponent {
+
+  @Input() component: any;
+
+}
