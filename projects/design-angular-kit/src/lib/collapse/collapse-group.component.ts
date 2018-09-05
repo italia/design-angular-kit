98808cff0baf2c5346b13289407eff840f5bb648
Pasquale02 <pasquale_florio@live.it>
2018-09-05 12:25:26 +0200

    feat(Collapse): Implementa gli eventi di notifica nei collapse innestati
    re #19

--- projects/design-angular-kit/src/lib/collapse/collapse-group.component.ts
+++ projects/design-angular-kit/src/lib/collapse/collapse-group.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ContentChildren, forwardRef, AfterContentInit, QueryList, Input, HostListener } from '@angular/core';
+import { Component, ContentChildren, forwardRef, QueryList,
+  Input, HostListener, SimpleChanges, OnChanges } from '@angular/core';
 import { CollapseItemComponent } from './collapse-item.component';
 import { Util } from '../util/util';
 
@@ -9,7 +10,7 @@ let identifier = 0;
   templateUrl: './collapse-group.component.html',
   styleUrls: ['./collapse-group.component.css']
 })
-export class CollapseGroupComponent implements AfterContentInit {
+export class CollapseGroupComponent implements OnChanges {
   id = `collapse-group-${identifier++}`;
 
   /**
@@ -23,8 +24,6 @@ export class CollapseGroupComponent implements AfterContentInit {
   @ContentChildren(forwardRef(() => CollapseItemComponent), { descendants: true })
   private _items: QueryList<CollapseItemComponent>; // tslint:disable-line
 
-  constructor() { }
-
   @HostListener('click', ['$event.target'])
   onClick(target) {
     if (this.accordion) {
@@ -32,14 +31,25 @@ export class CollapseGroupComponent implements AfterContentInit {
       items.forEach(currentItem => {
         const isTargetPartOfItem = (currentItem.elementRef.nativeElement as HTMLElement).contains(target);
         if (!isTargetPartOfItem) {
-          currentItem.directive.hide();
+          if (currentItem.directive.isShown()) {
+            currentItem.directive.hide();
+          }
         }
       });
     }
   }
 
-  ngAfterContentInit() {
-
+  ngOnChanges(simpleChanges: SimpleChanges) {
+    if (simpleChanges['accordion']) {
+      const isAccordion = simpleChanges['accordion'].currentValue;
+      if (isAccordion) {
+        const items = this._items.toArray();
+        items.forEach(currentItem => {
+          if (currentItem.directive.isShown()) {
+            currentItem.directive.hide();
+          }
+        });
+      }
+    }
   }
-
 }
--- projects/design-angular-kit/src/lib/collapse/collapse-item.component.ts
+++ projects/design-angular-kit/src/lib/collapse/collapse-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
 import { CollapseDirective } from './collapse.directive';
 
 let identifier = 0;
@@ -21,6 +21,54 @@ export class CollapseItemComponent {
   get header(): string { return this._header; }
   set header(value: string) { this._header = value; }
   private _header;
+
+  /**
+   * Evento da emettere quando il collapse sta per essere mostrato
+   */
+  @Output('show')
+  get showEvent(): EventEmitter<CollapseItemComponent> { return this._showEvent; }
+  set showEvent(value: EventEmitter<CollapseItemComponent>) { this._showEvent = value; }
+  private _showEvent = new EventEmitter<CollapseItemComponent>();
+
+  /**
+   * Evento da emettere quando il collapse è mostrato
+   */
+  @Output('shown')
+  get shownEvent(): EventEmitter<CollapseItemComponent> { return this._shownEvent; }
+  set shownEvent(value: EventEmitter<CollapseItemComponent>) { this._shownEvent = value; }
+  private _shownEvent = new EventEmitter<CollapseItemComponent>();
+
+  /**
+   * Evento da emettere quando il collapse sta per essere nascosto
+   */
+  @Output('hide')
+  get hideEvent(): EventEmitter<CollapseItemComponent> { return this._hideEvent; }
+  set hideEvent(value: EventEmitter<CollapseItemComponent>) { this._hideEvent = value; }
+  private _hideEvent = new EventEmitter<CollapseItemComponent>();
+
+  /**
+   * Evento da emettere quando il collapse è nascosto
+   */
+  @Output('hidden')
+  get hiddenEvent(): EventEmitter<CollapseItemComponent> { return this._hiddenEvent; }
+  set hiddenEvent(value: EventEmitter<CollapseItemComponent>) { this._hiddenEvent = value; }
+  private _hiddenEvent = new EventEmitter<CollapseItemComponent>();
+
+  show() {
+    this.showEvent.emit(this);
+  }
+
+  hide() {
+    this.hideEvent.emit(this);
+  }
+
+  shown() {
+    this.shownEvent.emit(this);
+  }
+
+  hidden() {
+    this.hiddenEvent.emit(this);
+  }
 
   constructor(readonly elementRef: ElementRef) { }
 }
