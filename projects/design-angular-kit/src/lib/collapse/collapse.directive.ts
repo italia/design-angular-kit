bbd4829c20e159a06de1a05c76c60db45f30d976
Your name here <critian.borelli@lascaux.it>
2022-09-03 14:07:05 +0200

    Collapse funzionante

--- projects/design-angular-kit/src/lib/collapse/collapse.directive.ts
+++ projects/design-angular-kit/src/lib/collapse/collapse.directive.ts
@@ -1,5 +1,6 @@
-import { Directive, Input, ElementRef, NgZone, OnInit, OnChanges, SimpleChanges} from '@angular/core';
+import { Directive, Input, ElementRef, NgZone, OnInit, OnChanges, SimpleChanges, ChangeDetectorRef, Output, EventEmitter} from '@angular/core';
 import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
+import { Util } from '../util/util';
 import { ItCollapseConfig } from './collapse.config';
 
 /**
@@ -13,28 +14,31 @@ import { ItCollapseConfig } from './collapse.config';
 })
 export class ItCollapseDirective extends NgbCollapse implements OnInit, OnChanges {
   
-  constructor(_elementRef: ElementRef<HTMLElement>, _config: ItCollapseConfig, _ngZone: NgZone) {
+  constructor(private _cd: ChangeDetectorRef,  _elementRef: ElementRef<HTMLElement>, _config: ItCollapseConfig, _ngZone: NgZone) {
       super(_elementRef, _config, _ngZone);
   }
 
   ngOnInit(): void {
-    super.ngOnInit();    
+    super.ngOnInit();
+    super.collapsed = Util.coerceBooleanProperty(this.itCollapse);
+    super.ngbCollapseChange = this.itCollapseChange;   
+    this._cd.detectChanges();
   }
 
   ngOnChanges({itCollapse}: SimpleChanges): void {
-      super.ngOnChanges({collapsed: itCollapse});
-      // Solo per il primo cambiamento è necessario aggiornare manualmente la proprietà collapsed in NgbCollapse, 
-      // altrimenti NgbCollapse.collapsed rimarrebbe con il valore di default.
-      if(itCollapse.firstChange) {
-        super.collapsed = this.itCollapse;
-      }
+    const coercedValue = Util.coerceBooleanProperty(itCollapse.currentValue);
+    super.collapsed = coercedValue;
+    super.ngOnChanges({collapsed: itCollapse});
+   
   }
 
   /**
    * La direttiva di collapse che opzionalmente contiene il predicato che ne stabilisce
    * che sarà avvalorato a true quando il collapse è espanso, a false altrimenti
    */
-  @Input() itCollapse: boolean;
+  @Input() itCollapse:boolean;
+  @Output() itCollapseChange = new EventEmitter<boolean>();
+
 
   get isCollapsed(): boolean {
     return this.collapsed;
