261e5583e6e1f21c813e2475f3ba433fe750d86b
Andrea Stagi <stagi.andrea@gmail.com>
2022-11-23 12:55:57 +0100

    chore: remove useless files from root

--- update-assets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Aggiorna i numeri di versione nei file package.json
-PACKAGE_VERSION=$(node -pe "require('./dist/design-angular-kit/package.json').version")
-sed -i -E -e "s/\"version\": \"[0-9]+\.[0-9]+\.[0-9]+\"/\"version\": \"${PACKAGE_VERSION}\"/g" ./package.json
-sed -i -E -e "s/\"version\": \"[0-9]+\.[0-9]+\.[0-9]+\"/\"version\": \"${PACKAGE_VERSION}\"/g" ./projects/design-angular-kit/package.json
-
-# Aggiunge il README.md al pacchetto NPM
-cp README.md dist/design-angular-kit/
