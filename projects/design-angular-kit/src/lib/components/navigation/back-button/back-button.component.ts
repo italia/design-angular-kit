52645ed24d8c4af0ba1d4adabcb2700833dc01a9
alenap93 <ilpugliese93@gmail.com>
2023-01-02 08:46:25 +0100

    Test: add test for back-button component

--- projects/design-angular-kit/src/lib/components/navigation/back-button/back-button.component.ts
+++ projects/design-angular-kit/src/lib/components/navigation/back-button/back-button.component.ts
@@ -1,51 +1,78 @@
 import { Component, Input } from '@angular/core';
 import { Location } from '@angular/common';
+import { IconColor, IconSize } from '../../../interfaces/icon';
+import { ButtonColor } from '../../../interfaces/core';
 
 @Component({
   selector: 'it-back-button',
-  templateUrl: './back-button.component.html',
-  styleUrls: ['./back-button.component.scss'],
-  exportAs: 'itUploadDragDrop'
+  templateUrl: './back-button.component.html'
 })
 export class BackButtonComponent {
 
   /**
-   * Back button style
-   * - <strong>link</strong>: use a link with icon and text
-   * - <strong>button</strong>: use a button with icon and text
-   * - <strong>icon</strong>: use a button with icon
-   * @default link
-   */
-  @Input() buttonStyle: 'link' | 'button' | 'icon' = 'link';
+   * Indica se va mostrato un link invece che un bottone.
+  */
+  @Input()
+  get isLink(): boolean { return this._isLink; }
+  set isLink(value: boolean) {
+    this._isLink = value;
+    if (value) {
+      this.iconColor = 'primary';
+    } else {
+      this.iconColor = 'white';
+    }
+  }
+  private _isLink = false;
+  iconColor: IconColor = 'white';
 
   /**
-   * Icon direction
-   * @default left
-   */
-  @Input() iconDirection: 'left' | 'up' = 'left';
+   * Indica se l'icona va mostrata o meno.
+  */
+  @Input()
+  get showIcon(): boolean { return this._showIcon; }
+  set showIcon(value: boolean) { this._showIcon = value; }
+  private _showIcon = true;
 
   /**
-   * Custom back logic <br/>
-   *
-   * NOTE: to use 'this' need bind function  <br/>
-   * @example backCbFn = this.errorCallback.bind(this);
-   * (errorCallback is your function, pass backCbFn to the component)
-   */
-  @Input() backFn?: (location: Location) => void;
+   * Indica se il testo va mostrato o meno.
+  */
+  @Input()
+  get showText(): boolean { return this._showText; }
+  set showText(value: boolean) { this._showText = value; }
+  private _showText = true;
 
-  constructor(
-    private readonly _location: Location
-  ) {
+  /**
+   * Indica se il tipo di BackButton è livello superiore o torna indietro.
+  */
+  @Input()
+  get type(): 'tornaIndietro' | 'livelloSuperiore' { return this._type; }
+  set type(value: 'tornaIndietro' | 'livelloSuperiore') {
+    this._type = value;
+    if (value === 'tornaIndietro') {
+      this.icon = 'arrow-left';
+      this.text = ' Torna indietro';
+    } else if (value === 'livelloSuperiore') {
+      this.icon = 'arrow-up';
+      this.text = ' Livello superiore';
+    }
+  }
+  private _type: 'tornaIndietro' | 'livelloSuperiore' = 'tornaIndietro';
+  icon = 'it-arrow-left';
+  text = 'Torna indietro';
+
+  iconSize: IconSize = 'sm';
+  buttonColor: ButtonColor = 'primary';
+
+  get iconClass(): string {
+    return this.showText ? 'me-2' : '';
   }
 
-  /**
-   * Go back function
-   */
-  public goBack(): void {
-    if (this.backFn) {
-      return this.backFn(this._location);
-    }
 
-    this._location.back();
+  constructor(
+    public location: Location
+  ) { }
+
+  goBack(): void {
+    this.location.back();
   }
 }
