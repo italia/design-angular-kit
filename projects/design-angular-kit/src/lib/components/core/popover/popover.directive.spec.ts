9cd5a9488cd288ff9624d67fa54c3badc8b71575
Matteo Pellegrini <61983612+Cruyff97@users.noreply.github.com>
2023-01-02 11:40:15 +0100

    chore(test): fix tests for new components

--- projects/design-angular-kit/src/lib/components/core/popover/popover.directive.spec.ts
+++ projects/design-angular-kit/src/lib/components/core/popover/popover.directive.spec.ts
@@ -1,8 +1,25 @@
-import { PopoverDirective } from './popover.directive';
+import { ElementRef } from '@angular/core'
+import { TestBed } from '@angular/core/testing'
+
+import { PopoverDirective } from './popover.directive'
+
+export class MockElementRef extends ElementRef {
+  nativeElement= {}
+}
 
 describe('PopoverDirective', () => {
+
+
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: ElementRef, useClass: MockElementRef }
+      ]
+    }).compileComponents();
+  })
+
   it('should create an instance', () => {
-    const directive = new PopoverDirective(null);
+    const directive = new PopoverDirective(TestBed.inject(ElementRef));
     expect(directive).toBeTruthy();
   });
 });
