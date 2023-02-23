5ba633991cfd2e2715c2ff4d922f546284aa3b97
Antonino Bonanno <bonanno@net-serv.it>
2023-02-23 11:03:17 +0100

    perf(fix): optimize and fix tab-component init using bootstrap

--- projects/design-angular-kit/src/lib/components/core/tab/tab-container/tab-container.component.ts
+++ projects/design-angular-kit/src/lib/components/core/tab/tab-container/tab-container.component.ts
@@ -1,12 +1,16 @@
-import { AfterViewInit, ChangeDetectorRef, Component, ContentChildren, Input, QueryList } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ContentChildren, Input, OnDestroy, QueryList } from '@angular/core';
 import { BooleanInput, isTrueBooleanInput } from '../../../../utils/boolean-input';
 import { TabItemComponent } from '../tab-item/tab-item.component';
+import { delay, startWith, Subscription, tap } from 'rxjs';
+import { Tab } from 'bootstrap-italia';
+import { AbstractComponent } from '../../../../abstracts/abstract.component';
 
 @Component({
   selector: 'it-tab-container',
-  templateUrl: './tab-container.component.html'
+  templateUrl: './tab-container.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TabContainerComponent implements AfterViewInit {
+export class TabContainerComponent extends AbstractComponent implements OnDestroy {
 
   /**
    * Tabs automatically occupy the entire available width
@@ -28,16 +32,43 @@ export class TabContainerComponent implements AfterViewInit {
    */
   @ContentChildren(TabItemComponent) tabs?: QueryList<TabItemComponent>;
 
-  constructor(
-    private readonly _changeDetectorRef: ChangeDetectorRef
-  ) {
-  }
+  private tabSubscriptions?: Array<Subscription>;
 
   isTrueBooleanInput(booleanInput?: BooleanInput): boolean {
     return isTrueBooleanInput(booleanInput);
   }
 
-  ngAfterViewInit(): void {
-    this._changeDetectorRef.detectChanges();
+  override ngAfterViewInit(): void {
+    super.ngAfterViewInit();
+
+    this.tabs?.changes.pipe( // When tabs changes (dynamic add/remove)
+      startWith(undefined),
+      tap(() => {
+        this.tabSubscriptions?.forEach(sub => sub.unsubscribe()); // Remove old subscriptions
+        this.tabSubscriptions = this.tabs?.map(tab => tab.valueChanges.subscribe(() => {
+          this._changeDetectorRef.detectChanges(); // DetectChanges when tab-item attributes changes
+        }));
+        this._changeDetectorRef.detectChanges(); // Force update html render
+      }),
+      delay(100)
+    ).subscribe(() => {
+      // Init tabs from bootstrap-italia
+      const triggerTabList: Array<HTMLAnchorElement> = this._elementRef.nativeElement.querySelectorAll('.nav-item a');
+      triggerTabList?.forEach(triggerEl => {
+        const tabTrigger = Tab.getOrCreateInstance(triggerEl);
+
+        if (triggerEl.getAttribute('tab-listener') !== 'true') {
+          triggerEl.addEventListener('click', event => {
+            event.preventDefault();
+            triggerEl.setAttribute('tab-listener', 'true'); // Prevents multiple insertion of the listener
+            tabTrigger.show();
+          });
+        }
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.tabSubscriptions?.forEach(sub => sub.unsubscribe());
   }
 }
