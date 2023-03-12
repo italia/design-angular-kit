0806ca19b5fe2e649ef357546aacc87d22f00299
Antonino Bonanno <nino.bonanno96@gmail.com>
2023-03-12 16:11:18 +0100

    feat: pagination added changer, pagination added jump to, pagination added text links, pagination added total number text, pagination added simple mode

--- projects/design-angular-kit/src/lib/components/core/pagination/pagination.component.ts
+++ projects/design-angular-kit/src/lib/components/core/pagination/pagination.component.ts
@@ -1,11 +1,22 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges
+} from '@angular/core';
+import { BooleanInput, isTrueBooleanInput } from '../../../utils/boolean-input';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, filter } from 'rxjs';
 
 @Component({
   selector: 'it-pagination[currentPage][pageNumbers]',
   templateUrl: './pagination.component.html',
-  styleUrls: ['./pagination.component.scss']
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PaginationComponent {
+export class PaginationComponent implements OnChanges {
 
   /**
    * Index of page (start 0)
@@ -18,26 +29,118 @@ export class PaginationComponent {
   @Input() pageNumbers!: number;
 
   /**
+   * Number of pages closest to the current one to display
+   * @default 5
+   */
+  @Input() visiblePages: number = 5;
+
+  /**
    * Pagination alignment (justify-content)
    */
   @Input() alignment?: 'center' | 'end';
 
   /**
-   * Fired when page is changed
+   * Enable/Disable simple mode
+   * Pagination in the "Simple mode" version is optimized for mobile devices.
+   * @default undefined - disabled
    */
-  @Output() newPageEvent = new EventEmitter<number>();
+  @Input() simpleMode?: BooleanInput;
 
   /**
-   * Create array to generate pagination of 5 element
+   * Enable/Disable text links
+   * Chevron icons used as navigation links are replaced by text links such as “previous” and “next”.
+   * @default undefined - disabled
    */
-  get pages(): Array<number> {
-    const length = this.pageNumbers > 5 ? 5 : this.pageNumbers;
-    let start = (this.currentPage > 1 && this.pageNumbers > 5) ? this.currentPage - 1 : 1;
+  @Input() textLinks?: BooleanInput;
 
-    if (this.pageNumbers > 5) {
+  /**
+   * Current value of Changer
+   * If is set show the Changer
+   * @default undefined - hide the Changer
+   */
+  @Input() currentChanger?: number;
+
+  /**
+   * Available Changer values
+   * @default [10, 25, 50, 100]
+   */
+  @Input() changerValues: Array<number> = [10, 25, 50, 100];
+
+  /**
+   * Hide/Show "Jump to page" input
+   * @default undefined - hidden
+   */
+  @Input() showJumpToPage?: BooleanInput;
+
+  /**
+   * Fired when page is changed. Emit the new index of page
+   */
+  @Output() pageEvent = new EventEmitter<number>();
+
+  /**
+   * Fired when changer is changed. Emit the new changer value
+   */
+  @Output() changerEvent = new EventEmitter<number>();
+
+  /**
+   * The pages
+   * @protected
+   */
+  protected pages: Array<number> = [];
+
+  /**
+   * Jump to page input
+   * @protected
+   */
+  protected jumpToPage: FormControl<number | null> = new FormControl<number | null>(null);
+
+  get isSimpleMode(): boolean {
+    return isTrueBooleanInput(this.simpleMode);
+  }
+
+  get isTextLinks(): boolean {
+    return isTrueBooleanInput(this.textLinks);
+  }
+
+  get isShowJumpToPage(): boolean {
+    return isTrueBooleanInput(this.showJumpToPage);
+  }
+
+  constructor() {
+    this.jumpToPage.valueChanges.pipe(
+      debounceTime(300), // Delay filter data after time span has passed without another source emission
+      distinctUntilChanged(),
+      filter(value => !!value && this.jumpToPage.valid)
+    ).subscribe(value => {
+      this.pageEvent.emit(value! - 1);
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.pages = this.calculatePages();
+    if (changes['currentPage']) {
+      this.jumpToPage.setValue(null, { emitEvent: false });
+    }
+  }
+
+  /**
+   * Create array to generate pagination of `visiblePages` element
+   */
+  private calculatePages(): Array<number> {
+    if (this.isSimpleMode) {
+      return [this.currentPage];
+    }
+
+    const length = this.pageNumbers > this.visiblePages ? this.visiblePages : this.pageNumbers;
+
+    const halfVisiblePages = Math.floor(this.visiblePages / 2);
+    let start = (this.currentPage > halfVisiblePages && this.pageNumbers > this.visiblePages) ?
+      this.currentPage - halfVisiblePages + 1 : 1;
+
+    if (this.pageNumbers > this.visiblePages) {
       if ((this.currentPage + 1) >= this.pageNumbers) {
-        start -= 2;
-      } else if (this.currentPage >= (this.pageNumbers - 2)) {
+        start -= halfVisiblePages;
+      } else if (this.currentPage >= (this.pageNumbers - halfVisiblePages)) {
         start -= (this.pageNumbers - (this.currentPage + 1));
       }
     }
@@ -47,10 +150,22 @@ export class PaginationComponent {
 
   /**
    * On click page change
+   * @param event click event
    * @param newPage the new page of table
    */
-  pageChange(newPage: number): void {
-    this.newPageEvent.emit(newPage - 1); // emit new page
+  protected pageChange(event: Event, newPage: number): void {
+    event.preventDefault();
+    this.pageEvent.emit(newPage - 1); // emit new page index
+  }
+
+  /**
+   * On click changer
+   * @param event click event
+   * @param value the new changer value
+   */
+  protected changerChange(event: Event, value: number): void {
+    event.preventDefault();
+    this.changerEvent.emit(value); // emit new changer value
   }
 
 }
