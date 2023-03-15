8d0e5cbe23582140c68cbcd16372d745f83642a4
Antonino Bonanno <nino.bonanno96@gmail.com>
2023-03-15 19:15:48 +0100

    feat: added ability to show or hide buttons in error page component

--- projects/design-angular-kit/src/lib/components/utils/error-page/error-page.component.ts
+++ projects/design-angular-kit/src/lib/components/utils/error-page/error-page.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BooleanInput, isTrueBooleanInput } from '../../../utils/boolean-input';
 
 @Component({
   selector: 'it-error-page',
@@ -14,15 +15,36 @@ export class ErrorPageComponent {
   @Input() errorCode?: number | 404 | 403 | 500;
 
   /**
+   * Show/Hide error code
+   * @default true - show
+   */
+  @Input() showErrorCode?: BooleanInput = true;
+
+  /**
    * Custom error title
+   * - If set it will be displayed instead of the default title.
+   * - It is possible to use i18n keys
    */
   @Input() errorTitle?: string;
 
   /**
    * Custom error description
+   * - If set it will be displayed instead of the default description.
+   * - It is possible to use i18n keys
    */
   @Input() errorDescription?: string;
 
+  /**
+   * Show/Hide back button
+   * @default true - show
+   */
+  @Input() showBackButton?: BooleanInput = true;
+
+  /**
+   * Show/Hide home button
+   * @default true - show
+   */
+  @Input() showHomeButton?: BooleanInput = true;
 
   constructor(
     private readonly route: ActivatedRoute
@@ -31,17 +53,38 @@ export class ErrorPageComponent {
       if (!this.errorCode && data['errorCode']) {
         this.errorCode = data['errorCode']; // Get errorCode from route data
       }
+      if (data['showErrorCode'] !== undefined) {
+        this.showBackButton = data['showErrorCode'];  // Get showErrorCode from route data
+      }
       if (!this.errorTitle && data['errorTitle']) {
         this.errorTitle = data['errorTitle'];  // Get errorTitle from route data
       }
       if (!this.errorDescription && data['errorDescription']) {
         this.errorDescription = data['errorDescription'];  // Get errorDescription from route data
       }
+      if (data['showBackButton'] !== undefined) {
+        this.showBackButton = data['showBackButton'];  // Get showBackButton from route data
+      }
+      if (data['showHomeButton'] !== undefined) {
+        this.showHomeButton = data['showHomeButton'];  // Get showHomeButton from route data
+      }
     });
   }
 
   get isDefaultErrorCode(): boolean {
     return this.errorCode === 404 || this.errorCode === 403 || this.errorCode === 500;
+  }
+
+  get isShowErrorCode(): boolean {
+    return isTrueBooleanInput(this.showErrorCode);
+  }
+
+  get isShowBackButton(): boolean {
+    return isTrueBooleanInput(this.showBackButton);
+  }
+
+  get isShowHomeButton(): boolean {
+    return isTrueBooleanInput(this.showHomeButton);
   }
 
 }
