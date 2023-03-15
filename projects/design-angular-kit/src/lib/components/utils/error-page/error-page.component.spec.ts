ee461020541ff605d6fb759555b9f60f2b39d310
Antonino Bonanno <nino.bonanno96@gmail.com>
2023-03-15 19:29:48 +0100

    fix: error page component test

--- projects/design-angular-kit/src/lib/components/utils/error-page/error-page.component.spec.ts
+++ projects/design-angular-kit/src/lib/components/utils/error-page/error-page.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ErrorPageComponent } from './error-page.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ErrorPageComponent', () => {
   let component: ErrorPageComponent;
@@ -8,9 +10,8 @@ describe('ErrorPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ErrorPageComponent ]
-    })
-    .compileComponents();
+      declarations: [ErrorPageComponent], imports: [TranslateModule.forRoot(), RouterTestingModule]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ErrorPageComponent);
     component = fixture.componentInstance;
