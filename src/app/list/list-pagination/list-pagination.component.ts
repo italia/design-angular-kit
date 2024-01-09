63d8b42e0ada53f751289a2c8de9e264c2bc3aae
Antonino Bonanno <nino.bonanno96@gmail.com>
2024-01-10 00:28:27 +0100

    feat(docs): added paginated list example

---
+++ src/app/list/list-pagination/list-pagination.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+interface PaginatedData {
+  currentPage: number,
+  pageNumbers: number,
+  data: Array<string>
+}
+
+@Component({
+  selector: 'it-list-pagination',
+  templateUrl: './list-pagination.component.html',
+  styleUrls: ['./list-pagination.component.scss']
+})
+export class ListPaginationComponent {
+  private pages: Array<Array<string>> = [
+    ['Antonino', 'Francesco', 'Giovanni'],
+    ['Linda', 'Riccardo', 'Marco'],
+    ['Daniel', 'Gioele', 'Giulio'],
+    ['Diego', 'Andrea', 'Lorenzo'],
+    ['Giorgio', 'Manuel', 'Luca'],
+  ]
+
+  protected selectedItem: PaginatedData = {
+    currentPage: 0,
+    pageNumbers: this.pages.length,
+    data: this.pages[0]
+  };
+
+  protected onPageChange(page: number): void {
+    this.selectedItem = {
+      ...this.selectedItem,
+      data: this.pages[page],
+      currentPage: page
+    };
+
+  }
+}
