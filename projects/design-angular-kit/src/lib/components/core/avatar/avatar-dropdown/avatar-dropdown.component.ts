0bbb7cb23df12920ded6b4b01db6a0a88872daa7
Antonino Bonanno <nino.bonanno96@gmail.com>
2024-01-14 11:39:14 +0100

    fix(avatar): fixed router-link missing importation

--- projects/design-angular-kit/src/lib/components/core/avatar/avatar-dropdown/avatar-dropdown.component.ts
+++ projects/design-angular-kit/src/lib/components/core/avatar/avatar-dropdown/avatar-dropdown.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, TemplateRef, ContentChildren, QueryList, Input, ViewEncapsulation } from '@angular/core';
 import { ItDropdownModule } from '../../dropdown/dropdown.module';
 import { NgForOf, NgIf, NgTemplateOutlet } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
@@ -36,7 +37,7 @@ export class ItAvatarDropdownItemComponent{
   templateUrl: './avatar-dropdown.component.html',
   styleUrls: ['./avatar-dropdown.component.scss'],
   host: { 'class': 'avatar avatar-dropdown' },
-  imports: [ItDropdownModule, NgTemplateOutlet, NgForOf, NgIf]
+  imports: [ItDropdownModule, NgTemplateOutlet, NgForOf, NgIf, RouterLink]
 })
 export class ItAvatarDropdownComponent{
   constructor() { }
