cc600e6a7600ac53bfc933b22d2750e71fffdcbc
Antonino Bonanno <nino.bonanno96@gmail.com>
2024-01-15 19:41:08 +0100

    refactor: refactor avatar component for no-host-metadata-property rule

--- projects/design-angular-kit/src/lib/components/core/avatar/avatar-dropdown/avatar-dropdown.component.ts
+++ projects/design-angular-kit/src/lib/components/core/avatar/avatar-dropdown/avatar-dropdown.component.ts
@@ -1,34 +1,37 @@
-import { Component, ViewChild, TemplateRef, ContentChildren, QueryList, Input } from '@angular/core';
+import { Component, ContentChildren, HostBinding, Input, QueryList, TemplateRef, ViewChild } from '@angular/core';
 import { ItDropdownModule } from '../../dropdown/dropdown.module';
 import { NgForOf, NgIf, NgTemplateOutlet } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
-
 @Component({
   standalone: true,
   selector: 'it-avatar-dropdown-item',
-  templateUrl: './avatar-dropdown-item.component.html',
+  template: '<ng-template><ng-content></ng-content></ng-template>',
   styleUrls: ['./avatar-dropdown.component.scss'],
   imports: [NgForOf, NgIf, NgTemplateOutlet]
 })
-export class ItAvatarDropdownItemComponent{
-  @ViewChild(TemplateRef, {static: true}) _implicitContent!: TemplateRef<any>
+export class ItAvatarDropdownItemComponent {
+  @ViewChild(TemplateRef, { static: true }) _implicitContent!: TemplateRef<any>;
+
   /**
    * Indica il link che possiamo passare all'elemento
    */
-  @Input() link?: string | any[] | null | undefined;
+  @Input() link: string | any[] | null | undefined;
+
   /**
    * Permette di utilizzare l'attributo html title
    */
-  @Input() title?: string
+  @Input() title?: string;
+
   /**
    * Permette di utilizzare l'attributo html accesskey
    */
-  @Input() accesskey?: string
-    /**
+  @Input() accesskey?: string;
+
+  /**
    * Permette di utilizzare l'attributo html tabindex
    */
-  @Input() tabindex?: number
+  @Input() tabindex?: number;
 }
 
 @Component({
@@ -36,10 +39,10 @@ export class ItAvatarDropdownItemComponent{
   selector: 'it-avatar-dropdown',
   templateUrl: './avatar-dropdown.component.html',
   styleUrls: ['./avatar-dropdown.component.scss'],
-  host: { 'class': 'avatar avatar-dropdown' },
   imports: [ItDropdownModule, NgTemplateOutlet, NgForOf, NgIf, RouterLink]
 })
-export class ItAvatarDropdownComponent{
-  constructor() { }
-  @ContentChildren(ItAvatarDropdownItemComponent) items! : QueryList<ItAvatarDropdownItemComponent>
+export class ItAvatarDropdownComponent {
+  @HostBinding('class') componentClass = 'avatar avatar-dropdown';
+
+  @ContentChildren(ItAvatarDropdownItemComponent) items!: QueryList<ItAvatarDropdownItemComponent>;
 }
