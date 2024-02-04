0388def994b0a3fcd127042f64599eb45b3d45eb
Antonino Bonanno <nino.bonanno96@gmail.com>
2024-02-04 21:09:25 +0100

    docs(sidebar): added docs for sidebar component
    Refs: #307

---
+++ src/app/sidebar/sidebar-dark-example/sidebar-dark-example.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'it-sidebar-dark-example',
+  templateUrl: './sidebar-dark-example.component.html',
+})
+export class SidebarDarkExampleComponent {}
---
+++ src/app/sidebar/sidebar-examples/sidebar-examples.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'it-sidebar-examples',
+  templateUrl: './sidebar-examples.component.html',
+})
+export class SidebarExamplesComponent {}
