7f5f4da357d9a1e975aa00f9dfb7f791a5f69ea4
Valerio <valeriocomo@users.noreply.github.com>
2024-02-26 12:42:17 +0100

    chore: devcontainer setup

---
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+echo "Set node_modules permission"
+sudo chown -R node:node node_modules
+echo "Set node_modules permission: done"
+
+echo "Installing deps"
+npm i
+echo "Installing deps: done"
