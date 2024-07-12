147b8f5089fc1edd8ce7dad6d6c510f2a1011a10
Antonino Bonanno <bonanno@net-serv.it>
2024-07-12 15:42:54 +0200

    feat(timeline): added timeline component
    Co-authored-by: Stefano Zanelli <stefano.zanelli@unitn.it>

---
+++ src/app/timeline/timeline-examples/timeline-examples.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'it-timeline-examples',
+  templateUrl: './timeline-examples.component.html',
+})
+export class TimelineExamplesComponent {}
