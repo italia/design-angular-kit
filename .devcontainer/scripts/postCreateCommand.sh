1211e65f76595806f86c88a4bbf8f8c00915bedc
valerio como <valeriocomo@gmail.com>
2024-07-18 16:32:14 +0000

    chore: devcontainer fix

--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -1,9 +1,12 @@
 #/bin/bash
+echo "lifecycle hook: postCreateCommand => start"
 
-echo "Set node_modules permission"
-sudo chown -R node:node node_modules
-echo "Set node_modules permission: done"
+# echo "Set node_modules permission"
+# sudo chown -R node:node node_modules
+# echo "Set node_modules permission: done"
 
 echo "Installing deps"
 npm i
 echo "Installing deps: done"
+
+echo "lifecycle hook: postCreateCommand => done"
---
+++ .devcontainer/scripts/postStartCommand.sh
@@ -0,0 +1,4 @@
+echo "lifecycle hook: postStartCommand => start"
+# git config --global --add safe.directory ${containerWorkspaceFolder}
+git config --global --add safe.directory /workspaces/design-angular-kit
+echo "lifecycle hook: postStartCommand => done"
