5126e5c66c924141fafa3af250159217ac9f2772
valerio como <valeriocomo@gmail.com>
2024-07-22 08:33:31 +0000

    chore: setup examples

---
+++ src/app/transfer/transfer-default-example/transfer-default-example.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'it-transfer-default-example',
+  templateUrl: './transfer-default-example.component.html',
+})
+export class TransferDefaultExampleComponent {}
--- src/app/transfer/transfer.module.ts
+++ src/app/transfer/transfer.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { TransferRoutingModule } from './transfer-routing.module';
 import { TransferIndexComponent } from './transfer-index/transfer-index.component';
 import { SharedModule } from '../shared/shared.module';
+import { TransferDefaultExampleComponent } from './transfer-default-example/transfer-default-example.component';
 
 @NgModule({
-  declarations: [TransferIndexComponent],
+  declarations: [TransferIndexComponent, TransferDefaultExampleComponent],
   imports: [TransferRoutingModule, SharedModule],
 })
 export class TransferModule {}
