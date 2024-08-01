3b290710af7588ccf508b223bc4b380a475486f6
valerio como <v.como@innovazione.gov.it>
2024-08-01 08:37:43 +0000

    chore: header

--- projects/design-angular-kit/src/lib/components/navigation/navscroll/navscroll.component.ts
+++ projects/design-angular-kit/src/lib/components/navigation/navscroll/navscroll.component.ts
@@ -1,5 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+interface NavscrollItem {
+  title: string;
+  text: string;
+  childs: NavscrollItems;
+}
+
+type NavscrollItems = Array<NavscrollItem>;
+
 @Component({
   selector: 'it-navscroll',
   standalone: true,
@@ -8,4 +16,45 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './navscroll.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ItNavscrollComponent {}
+export class ItNavscrollComponent {
+  readonly header = 'Header';
+  readonly items = [
+    {
+      title: 'Prima Sezione',
+      text: 'Testo prima sezione',
+      childs: [
+        {
+          title: 'Sottosezione 1.1',
+          text: 'Testo sottosezione 1.1',
+          childs: [],
+        },
+        {
+          title: 'Sottosezione 1.2',
+          text: 'Testo sottosezione 1.2',
+          childs: [],
+        },
+        {
+          title: 'Sottosezione 1.3',
+          text: 'Testo sottosezione 1.3',
+          childs: [],
+        },
+      ],
+    },
+    {
+      title: 'Seconda Sezione',
+      text: 'Testo seconda sezione',
+      childs: [
+        {
+          title: 'Sottosezione 2.1',
+          text: 'Testo sottosezione 2.1',
+          childs: [],
+        },
+        {
+          title: 'Sottosezione 2.2',
+          text: 'Testo sottosezione 2.2',
+          childs: [],
+        },
+      ],
+    },
+  ] satisfies NavscrollItems;
+}
