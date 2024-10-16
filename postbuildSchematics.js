9398dc7fb7b8fed53c82ee9f8c52958fd25358c4
valerio como <v.como@innovazione.gov.it>
2024-10-16 16:05:27 +0000

    chore: rework

--- postbuildSchematics.js
+++ postbuildSchematics.js
@@ -12,11 +12,13 @@ var callback = function(e)  {
 //copyfiles -f projects/design-angular-kit/schematics/collection.json dist/design-angular-kit/schematics/
 var collectionSourcePath = 'projects/design-angular-kit/schematics/collection.json';
 var collectionDestionationPath = 'dist/design-angular-kit/schematics/';
-copyfiles([collectionSourcePath, collectionDestionationPath], flat, callback);
+var collectionConfig =  {verbose: true, flat: true, up: 3};
+copyfiles([collectionSourcePath, collectionDestionationPath],collectionConfig, callback);
 
 // COPY schematics/*/schema.json
 
 //copyfiles -f projects/design-angular-kit/schematics/*/schema.json dist/design-angular-kit/"
 var schemajsonSourcePath = 'projects/design-angular-kit/schematics/*/schema.json';
 var schemajsonDestionationPath = 'dist/design-angular-kit/';
-copyfiles([schemajsonSourcePath, schemajsonDestionationPath], {verbose: true, flat: false, up: 2}, callback);
+var schemajsonConfig =  {verbose: true, flat: false, up: 2};
+copyfiles([schemajsonSourcePath, schemajsonDestionationPath], schemajsonConfig, callback);
