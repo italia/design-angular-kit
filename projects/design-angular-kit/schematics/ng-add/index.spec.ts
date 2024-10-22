7c8abd32ebe66a350c2283379a493568305a3ff3
valerio como <v.como@innovazione.gov.it>
2024-10-22 14:37:29 +0000

    chore: setup test schematics

---
+++ projects/design-angular-kit/schematics/ng-add/index.spec.ts
@@ -0,0 +1,45 @@
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
+import * as path from 'path';
+import { Schema as SchematicOptions } from './schema';
+
+describe(`ng add design-angular-kit - NgModule`, () => {
+  const collectionPath = path.join(__dirname, '../collection.json');
+  const runner = new SchematicTestRunner('schematics', collectionPath);
+
+  const defaultOptions: SchematicOptions = {
+    project: 'test-project', // Set your default project name
+  };
+
+  let appTree: UnitTestTree;
+
+  beforeEach(async () => {
+    // Simulate an Angular workspace project before running the schematic
+    appTree = await runner.runExternalSchematic('@schematics/angular', 'workspace', {
+      name: 'workspace',
+      version: '6.0.0',
+      newProjectRoot: 'projects',
+    });
+
+    appTree = await runner.runExternalSchematic(
+      '@schematics/angular',
+      'application',
+      {
+        name: defaultOptions.project,
+        standalone: false,
+      },
+      appTree
+    );
+  });
+
+  it('should add DesignAngularKitModule to the root module', async () => {
+    const tree = await runner.runSchematic('ng-add', defaultOptions, appTree);
+
+    // Check if the app.module.ts file exists
+    const modulePath = '/projects/test-project/src/app/app.module.ts';
+    expect(tree.files).toContain(modulePath);
+
+    // Check if the correct import statement was added
+    const content = tree.readContent(modulePath);
+    expect(content).toContain(`import { DesignAngularKitModule } from 'design-angular-kit';`);
+  });
+});
--- projects/design-angular-kit/schematics/ng-add/index.ts
+++ projects/design-angular-kit/schematics/ng-add/index.ts
@@ -2,7 +2,7 @@ import { Rule } from '@angular-devkit/schematics';
 import { addRootImport } from '@schematics/angular/utility';
 import { Schema } from './schema';
 
-export function ngAdd(options: Schema): Rule {
+export default function (options: Schema): Rule {
   // Add an import `DesignAngularKitModule` from `design-angular-kit` to the root of the user's project.
   return addRootImport(options.project, ({ code, external }) => code`${external('DesignAngularKitModule', 'design-angular-kit')}`);
 }
