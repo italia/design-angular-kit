4661227c61804ea7375042590e8deac58129d9e5
valerio como <v.como@innovazione.gov.it>
2024-10-23 13:00:18 +0000

    feat: standalone support

--- projects/design-angular-kit/schematics/ng-add/index.spec.ts
+++ projects/design-angular-kit/schematics/ng-add/index.spec.ts
@@ -44,3 +44,46 @@ describe(`ng add design-angular-kit - NgModule`, () => {
     expect(content).toContain(`DesignAngularKitModule.forRoot()`);
   });
 });
+
+describe(`ng add design-angular-kit - standalone`, () => {
+  const collectionPath = path.join(__dirname, '../collection.json');
+  const runner = new SchematicTestRunner('schematics', collectionPath);
+
+  const defaultOptions: SchematicOptions = {
+    project: 'test-project', // Set your default project name
+  };
+
+  let appTree: UnitTestTree;
+
+  beforeEach(async () => {
+    // Simulate an Angular workspace project before running the schematic
+    appTree = await runner.runExternalSchematic('@schematics/angular', 'workspace', {
+      name: 'workspace',
+      version: '6.0.0',
+      newProjectRoot: 'projects',
+    });
+
+    appTree = await runner.runExternalSchematic(
+      '@schematics/angular',
+      'application',
+      {
+        name: defaultOptions.project,
+        standalone: true,
+      },
+      appTree
+    );
+  });
+
+  it('should add provideDesignAngularKit() to the app config', async () => {
+    const tree = await runner.runSchematic('ng-add', defaultOptions, appTree);
+
+    // Check if the app.module.ts file exists
+    const appConfigPath = '/projects/test-project/src/app/app.config.ts';
+    expect(tree.files).toContain(appConfigPath);
+
+    // Check if the correct import statement was added
+    const content = tree.readContent(appConfigPath);
+    expect(content).toContain(`import { provideDesignAngularKit } from 'design-angular-kit';`);
+    expect(content).toContain(`provideDesignAngularKit()`);
+  });
+});
--- projects/design-angular-kit/schematics/ng-add/index.ts
+++ projects/design-angular-kit/schematics/ng-add/index.ts
@@ -1,11 +1,50 @@
-import { Rule } from '@angular-devkit/schematics';
-import { addRootImport } from '@schematics/angular/utility';
+import { callRule, chain, Rule, SchematicContext, SchematicsException, Tree } from '@angular-devkit/schematics';
+import { addRootImport, addRootProvider, readWorkspace } from '@schematics/angular/utility';
+import { isStandaloneApp } from '@schematics/angular/utility/ng-ast-utils';
+import { getMainFilePath } from '@schematics/angular/utility/standalone/util';
+
 import { Schema } from './schema';
 
 export default function (options: Schema): Rule {
   // Add an import `DesignAngularKitModule` from `design-angular-kit` to the root of the user's project.
-  return addRootImport(
-    options.project,
-    ({ code, external }) => code`${external('DesignAngularKitModule', 'design-angular-kit')}.forRoot(),\n`
-  );
+  // return addRootImport(
+  //   options.project,
+  //   ({ code, external }) => code`${external('DesignAngularKitModule', 'design-angular-kit')}.forRoot(),\n`
+  // );
+  return async (host: Tree) => {
+    const workspace = await readWorkspace(host);
+    const projectName = options.project || (workspace.extensions.defaultProject as string);
+
+    // 1. getting project by name
+    const project = workspace.projects.get(projectName);
+    if (!project) {
+      throw new SchematicsException('messages.noProject(projectName)');
+    }
+
+    // 2. getting main file for project
+    const mainFilePath = await getMainFilePath(host, projectName);
+    if (!mainFilePath || !host.read(mainFilePath)) {
+      throw new SchematicsException('messages.noMainFile(projectName)');
+    }
+
+    const isNotStandaloneApp = !isStandaloneApp(host, mainFilePath);
+
+    console.log(isNotStandaloneApp);
+
+    return chain([addDesignAngularKit({ mainFilePath, projectName })]);
+  };
+}
+
+function addDesignAngularKit({ mainFilePath, projectName }: { mainFilePath: string; projectName: string }): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    const isNotStandaloneApp = !isStandaloneApp(host, mainFilePath);
+
+    console.log(isNotStandaloneApp);
+
+    const rule = isNotStandaloneApp
+      ? addRootImport(projectName, ({ code, external }) => code`${external('DesignAngularKitModule', 'design-angular-kit')}.forRoot()\n`)
+      : addRootProvider(projectName, ({ code, external }) => code`${external('provideDesignAngularKit', 'design-angular-kit')}()`);
+
+    return callRule(rule, host, context);
+  };
 }
