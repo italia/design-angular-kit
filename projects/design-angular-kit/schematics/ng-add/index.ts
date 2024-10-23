b74c70911f44aed89f8f6e3af4800e64e8fd1ebb
valerio como <v.como@innovazione.gov.it>
2024-10-23 15:43:59 +0000

    feat: added test for package.json

--- projects/design-angular-kit/schematics/ng-add/index.ts
+++ projects/design-angular-kit/schematics/ng-add/index.ts
@@ -1,39 +1,63 @@
-import { callRule, chain, Rule, SchematicContext, SchematicsException, Tree } from '@angular-devkit/schematics';
-import { addRootImport, addRootProvider, readWorkspace } from '@schematics/angular/utility';
-import { isStandaloneApp } from '@schematics/angular/utility/ng-ast-utils';
-import { getMainFilePath } from '@schematics/angular/utility/standalone/util';
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { NodePackageInstallTask, RunSchematicTask } from '@angular-devkit/schematics/tasks';
 
+import { addPackageToPackageJson, getPackageVersionFromPackageJson, toSemVerObject } from './package-config';
 import { Schema } from './schema';
 
+const fallbackMaterialVersionRange = `~0.0.0-PLACEHOLDER`;
+const packageName = 'design-angular-kit';
+
+const ANGULAR_VERSION_17 = '17';
+const ANGULAR_VERSION_18 = '18';
+
+const KIT_VERSION_1_0_0 = '1.0.0';
+const KIT_VERSION_1_1_0 = '1.1.0';
+const DEFAULT = 'DEFAULT';
+
+const versionsMap: Record<string, string> = {
+  [DEFAULT]: KIT_VERSION_1_0_0,
+  [ANGULAR_VERSION_17]: KIT_VERSION_1_0_0,
+  [ANGULAR_VERSION_18]: KIT_VERSION_1_1_0,
+};
+
 export default function (options: Schema): Rule {
-  return async (host: Tree) => {
-    const workspace = await readWorkspace(host);
-    const projectName = options.project || (workspace.extensions.defaultProject as string);
-
-    // 1. getting project by name
-    const project = workspace.projects.get(projectName);
-    if (!project) {
-      throw new SchematicsException('messages.noProject(projectName)');
-    }
-
-    // 2. getting main file for project
-    const mainFilePath = await getMainFilePath(host, projectName);
-    if (!mainFilePath || !host.read(mainFilePath)) {
-      throw new SchematicsException('messages.noMainFile(projectName)');
-    }
-
-    return chain([addDesignAngularKit({ mainFilePath, projectName })]);
-  };
-}
-
-function addDesignAngularKit({ mainFilePath, projectName }: { mainFilePath: string; projectName: string }): Rule {
   return (host: Tree, context: SchematicContext) => {
-    const isNotStandaloneApp = !isStandaloneApp(host, mainFilePath);
+    // Version tag of the `@angular/core` dependency that has been loaded from the `package.json`
+    // of the CLI project. This tag should be preferred because all Angular dependencies should
+    // have the same version tag if possible.
+    const ngCoreVersionTag = getPackageVersionFromPackageJson(host, '@angular/core');
+    const designAngularKitVersionRange = getPackageVersionFromPackageJson(host, packageName);
+    const angularDependencyVersion = ngCoreVersionTag || `0.0.0-NG`;
 
-    const rule = isNotStandaloneApp
-      ? addRootImport(projectName, ({ code, external }) => code`${external('DesignAngularKitModule', 'design-angular-kit')}.forRoot()\n`)
-      : addRootProvider(projectName, ({ code, external }) => code`${external('provideDesignAngularKit', 'design-angular-kit')}()`);
+    const { major: angularMajorVersion } = toSemVerObject(angularDependencyVersion);
 
-    return callRule(rule, host, context);
+    let version = versionsMap[DEFAULT];
+
+    switch (angularMajorVersion) {
+      case ANGULAR_VERSION_17:
+        version = versionsMap[ANGULAR_VERSION_17];
+        break;
+      case ANGULAR_VERSION_18:
+        version = versionsMap[ANGULAR_VERSION_18];
+        break;
+      default:
+        version = versionsMap[DEFAULT];
+    }
+
+    // The CLI inserts `design-angular-kit` into the `package.json` before this schematic runs.
+    // This means that we do not need to insert Design Angular Kit into `package.json` files again.
+    // In some cases though, it could happen that this schematic runs outside of the CLI `ng add`
+    // command, or Design Angular Kit is only listed a dev dependency. If that is the case, we insert a
+    // version based on the current build version (substituted version placeholder).
+    if (designAngularKitVersionRange === null) {
+      addPackageToPackageJson(host, packageName, version);
+    }
+
+    addPackageToPackageJson(host, '@angular/cdk', angularDependencyVersion || fallbackMaterialVersionRange);
+    addPackageToPackageJson(host, '@angular/animations', angularDependencyVersion);
+
+    const installTaskId = context.addTask(new NodePackageInstallTask());
+
+    context.addTask(new RunSchematicTask('ng-add-setup-project', options), [installTaskId]);
   };
 }
---
+++ projects/design-angular-kit/schematics/ng-add/package-config.ts
@@ -0,0 +1,63 @@
+import { Tree } from '@angular-devkit/schematics';
+
+interface PackageJson {
+  dependencies: Record<string, string>;
+}
+
+// interface SemVerObject {}
+
+function sortObjectByKeys(obj: Record<string, string>) {
+  return Object.keys(obj)
+    .sort()
+    .reduce(
+      (result, key) => {
+        result[key] = obj[key];
+        return result;
+      },
+      {} as Record<string, string>
+    );
+}
+
+const semverWithPrefixRegex =
+  /^[\^~]?(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9A-Za-z-][0-9A-Za-z-]*)(?:\.(?:0|[1-9A-Za-z-][0-9A-Za-z-]*))*))?(?:\+([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?$/;
+
+export function toSemVerObject(versionString: string) {
+  const [semVer, major, minor, patch, prerelease, buildmetadata] = versionString.match(semverWithPrefixRegex) ?? [];
+  return { semVer, major, minor, patch, prerelease, buildmetadata };
+}
+
+/** Adds a package to the package.json in the given host tree. */
+export function addPackageToPackageJson(host: Tree, pkg: string, version: string): Tree {
+  if (host.exists('package.json')) {
+    const sourceText = host.read('package.json')!.toString('utf-8');
+    const json = JSON.parse(sourceText) as PackageJson;
+
+    if (!json.dependencies) {
+      json.dependencies = {};
+    }
+
+    if (!json.dependencies[pkg]) {
+      json.dependencies[pkg] = version;
+      json.dependencies = sortObjectByKeys(json.dependencies);
+    }
+
+    host.overwrite('package.json', JSON.stringify(json, null, 2));
+  }
+
+  return host;
+}
+
+/** Gets the version of the specified package by looking at the package.json in the given tree. */
+export function getPackageVersionFromPackageJson(tree: Tree, name: string): string | null {
+  if (!tree.exists('package.json')) {
+    return null;
+  }
+
+  const packageJson = JSON.parse(tree.read('package.json')!.toString('utf8')) as PackageJson;
+
+  if (packageJson.dependencies && packageJson.dependencies[name]) {
+    return packageJson.dependencies[name];
+  }
+
+  return null;
+}
