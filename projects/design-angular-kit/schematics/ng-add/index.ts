2bca342fbfe025c74557a8487aa4b6139244e9df
valerio como <v.como@innovazione.gov.it>
2024-10-29 14:21:28 +0000

    chore: rework comments

--- projects/design-angular-kit/schematics/ng-add/index.ts
+++ projects/design-angular-kit/schematics/ng-add/index.ts
@@ -6,27 +6,24 @@ import { addPackageToPackageJson, getPackageVersionFromPackageJson, toSemVerObje
 import { getPackageVersion } from './versions-helper';
 
 const DESIGN_ANGULAR_KIT_PACKAGE_NAME = 'design-angular-kit';
+const NG_LATEST_VERSION = '19.0.0';
 
 export default function (options: Schema): Rule {
   return (host: Tree, context: SchematicContext) => {
-    // Version tag of the `@angular/core` dependency that has been loaded from the `package.json`
-    // of the CLI project. This tag should be preferred because all Angular dependencies should
-    // have the same version tag if possible.
+    // Get the version of `@angular/core`
+    // This tag should be preferred because all Angular dependencies should  have the same version tag if possible.
     const ngCoreVersionTag = getPackageVersionFromPackageJson(host, '@angular/core');
     const designAngularKitVersionRange = getPackageVersionFromPackageJson(host, DESIGN_ANGULAR_KIT_PACKAGE_NAME);
-    const angularDependencyVersion = ngCoreVersionTag || `18.0.0`;
+    const angularDependencyVersion = ngCoreVersionTag || NG_LATEST_VERSION;
 
     const { major: angularMajorVersion } = toSemVerObject(angularDependencyVersion);
 
-    const version = getPackageVersion({ angularMajorVersion });
+    const designAngularKitPackageVersion = getPackageVersion({ angularMajorVersion });
 
     // The CLI inserts `design-angular-kit` into the `package.json` before this schematic runs.
     // This means that we do not need to insert Design Angular Kit into `package.json` files again.
-    // In some cases though, it could happen that this schematic runs outside of the CLI `ng add`
-    // command, or Design Angular Kit is only listed a dev dependency. If that is the case, we insert a
-    // version based on the current build version (substituted version placeholder).
     if (designAngularKitVersionRange === null) {
-      addPackageToPackageJson(host, DESIGN_ANGULAR_KIT_PACKAGE_NAME, version);
+      addPackageToPackageJson(host, DESIGN_ANGULAR_KIT_PACKAGE_NAME, designAngularKitPackageVersion);
     }
 
     addPackageToPackageJson(host, '@angular/cdk', angularDependencyVersion);
