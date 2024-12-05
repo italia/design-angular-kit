37a63f021e7e1babbb6d8b6a05f5ab92d3472c43
Valerio Como <v.como@innovazione.gov.it>
2024-12-05 11:07:27 +0000

    chore: version helper tests

---
+++ projects/design-angular-kit/schematics/ng-add/versions-helper.spec.ts
@@ -0,0 +1,30 @@
+import { getPackageVersion } from './versions-helper';
+
+fdescribe('schematics versions helper tests', () => {
+  it('should return v1.0.0 for Angular v17', () => {
+    //arrange
+    const angularMajorVersion = '17';
+    //act
+    const actual = getPackageVersion({ angularMajorVersion });
+    //assert
+    expect(actual).toBe('1.0.0');
+  });
+
+  it('should return v1.1.0 for Angular v18', () => {
+    //arrange
+    const angularMajorVersion = '18';
+    //act
+    const actual = getPackageVersion({ angularMajorVersion });
+    //assert
+    expect(actual).toBe('1.1.0');
+  });
+
+  it('should return v1.1.0 for Angular v19', () => {
+    //arrange
+    const angularMajorVersion = '19';
+    //act
+    const actual = getPackageVersion({ angularMajorVersion });
+    //assert
+    expect(actual).toBe('1.1.0');
+  });
+});
