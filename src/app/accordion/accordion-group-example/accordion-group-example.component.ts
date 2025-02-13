7fc167e0f732045bf5d245213db667cd4ef69bec
Andrea Stagi <stagi.andrea@gmail.com>
2025-02-13 18:58:36 +0100

    docs: accordion has its own documentation now

---
+++ src/app/accordion/accordion-group-example/accordion-group-example.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'it-accordion-group-example',
+  templateUrl: './accordion-group-example.component.html',
+  styleUrls: ['./accordion-group-example.component.scss'],
+})
+export class AccordionGroupExampleComponent {}
--- src/app/collapse/collapse.module.ts
+++ src/app/collapse/collapse.module.ts
@@ -7,17 +7,9 @@ import { CollapseExamplesComponent } from './collapse-examples/collapse-examples
 import { CollapseIndexComponent } from './collapse-index/collapse-index.component';
 import { SharedModule } from '../shared/shared.module';
 import { FormsModule } from '@angular/forms';
-import { CollapseGroupExampleComponent } from './collapse-group-example/collapse-group-example.component';
-import { CollapseNestedExampleComponent } from './collapse-nested-example/collapse-nested-example.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, SharedModule, CollapseRoutingModule],
-  declarations: [
-    CollapseExampleComponent,
-    CollapseExamplesComponent,
-    CollapseIndexComponent,
-    CollapseGroupExampleComponent,
-    CollapseNestedExampleComponent,
-  ],
+  declarations: [CollapseIndexComponent, CollapseExampleComponent, CollapseExamplesComponent],
 })
 export class CollapseModule {}
