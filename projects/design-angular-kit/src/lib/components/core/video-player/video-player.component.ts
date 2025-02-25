4091ef055f6028df364e6d19ba42fb41c3ae7168
Valerio Como <v.como@innovazione.gov.it>
2025-02-25 15:39:02 +0000

    feat: generic title and transcription

--- projects/design-angular-kit/src/lib/components/core/video-player/video-player.component.ts
+++ projects/design-angular-kit/src/lib/components/core/video-player/video-player.component.ts
@@ -1,4 +1,4 @@
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgTemplateOutlet } from '@angular/common';
 import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import videojs from 'video.js';
@@ -21,91 +21,69 @@ enum ViewType {
   standalone: true,
   selector: 'it-video-player',
   template: `@switch (viewType$ | async) {
-    @case (viewTypes.Default) {
-      <div class="row">
-        <video #videoPlayer class="video-js vjs-theme-bootstrap-italia vjs-fluid vjs-big-play-centered"></video>
-        <div class="vjs-transcription accordion">
-          <div class="accordion-item">
-            <h2 class="accordion-header " id="transcription-{{ id }}-head">
-              <button
-                class="accordion-button collapsed"
-                type="button"
-                data-bs-toggle="collapse"
-                [attr.data-bs-target]="'#transcription-' + id"
-                [attr.aria-controls]="'transcription-' + id"
-                aria-expanded="true">
-                Trascrizione
-              </button>
-            </h2>
-            <div
-              id="transcription-{{ id }}"
-              class="accordion-collapse collapse"
-              role="region"
-              [attr.aria-labelledby]="'transcription-' + id + '-head'">
-              <div class="accordion-body">
-                Vestibulum hendrerit ultrices nibh, sed pharetra lacus ultrices eget. Morbi et ipsum et sapien dapibus facilisis. Integer
-                eget semper nibh. Proin enim nulla, egestas ac rutrum eget, ullamcorper nec turpis.
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    }
-    @case (viewTypes.Overlay) {
-      <div #acceptOverlayable class="acceptoverlayable">
-        <div #acceptOveraly class="acceptoverlay acceptoverlay-primary fade show">
-          <div class="acceptoverlay-inner">
-            <div class="acceptoverlay-icon">
-              <svg class="icon icon-xl"><use href="/bootstrap-italia/dist/svg/sprites.svg#it-video"></use></svg>
-            </div>
-            <p>
-              Accetta i cookie di YouTube per vedere il video. Puoi gestire le preferenze nella
-              <a href="#" class="text-white">cookie policy</a>.
-            </p>
-            <div class="acceptoverlay-buttons bg-dark">
-              <button type="button" class="btn btn-primary" (click)="acceptCookieHandler()">Accetta</button>
-              <div class="form-check">
-                <input id="chk-remember" type="checkbox" #chkRemember />
-                <label for="chk-remember">Ricorda per tutti i video</label>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div>
+      @case (viewTypes.Default) {
+        <div class="row">
           <video #videoPlayer class="video-js vjs-theme-bootstrap-italia vjs-fluid vjs-big-play-centered"></video>
-          <div class="vjs-transcription accordion">
-            <div class="accordion-item">
-              <h2 class="accordion-header " id="transcription-{{ id }}-head">
-                <button
-                  class="accordion-button collapsed"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  [attr.data-bs-target]="'#transcription-' + id"
-                  [attr.aria-controls]="'transcription-' + id"
-                  aria-expanded="true">
-                  Trascrizione
-                </button>
-              </h2>
-              <div
-                id="transcription-{{ id }}"
-                class="accordion-collapse collapse"
-                role="region"
-                [attr.aria-labelledby]="'transcription-' + id + '-head'">
-                <div class="accordion-body">
-                  Vestibulum hendrerit ultrices nibh, sed pharetra lacus ultrices eget. Morbi et ipsum et sapien dapibus facilisis. Integer
-                  eget semper nibh. Proin enim nulla, egestas ac rutrum eget, ullamcorper nec turpis.
+          <ng-container *ngTemplateOutlet="transcriptionTemplate"></ng-container>
+        </div>
+      }
+      @case (viewTypes.Overlay) {
+        <div #acceptOverlayable class="acceptoverlayable">
+          <div #acceptOveraly class="acceptoverlay acceptoverlay-primary fade show">
+            <div class="acceptoverlay-inner">
+              <div class="acceptoverlay-icon">
+                <svg class="icon icon-xl"><use href="/bootstrap-italia/dist/svg/sprites.svg#it-video"></use></svg>
+              </div>
+              <p>
+                Accetta i cookie di YouTube per vedere il video. Puoi gestire le preferenze nella
+                <a href="#" class="text-white">cookie policy</a>.
+              </p>
+              <div class="acceptoverlay-buttons bg-dark">
+                <button type="button" class="btn btn-primary" (click)="acceptCookieHandler()">Accetta</button>
+                <div class="form-check">
+                  <input id="chk-remember" type="checkbox" #chkRemember />
+                  <label for="chk-remember">Ricorda per tutti i video</label>
                 </div>
               </div>
             </div>
           </div>
+          <div>
+            <video #videoPlayer class="video-js vjs-theme-bootstrap-italia vjs-fluid vjs-big-play-centered"></video>
+            <ng-container *ngTemplateOutlet="transcriptionTemplate"></ng-container>
+          </div>
+        </div>
+      }
+      @default {
+        <h1>No video provider</h1>
+      }
+    }
+    <ng-template #transcriptionTemplate>
+      <div class="vjs-transcription accordion">
+        <div class="accordion-item">
+          <h2 class="accordion-header " id="transcription-{{ id }}-head">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              [attr.data-bs-target]="'#transcription-' + id"
+              [attr.aria-controls]="'transcription-' + id"
+              aria-expanded="true">
+              <ng-content select="[transcriptionTitle]">Trascrizione</ng-content>
+            </button>
+          </h2>
+          <div
+            id="transcription-{{ id }}"
+            class="accordion-collapse collapse"
+            role="region"
+            [attr.aria-labelledby]="'transcription-' + id + '-head'">
+            <div class="accordion-body">
+              <ng-content select="[transcriptionText]">-</ng-content>
+            </div>
+          </div>
         </div>
       </div>
-    }
-    @default {
-      <h1>No video provider</h1>
-    }
-  }`,
-  imports: [AsyncPipe],
+    </ng-template> `,
+  imports: [AsyncPipe, NgTemplateOutlet],
   encapsulation: ViewEncapsulation.None,
 })
 export class ItVideoPlayerComponent extends ItAbstractComponent implements OnInit, AfterViewInit, OnDestroy {
