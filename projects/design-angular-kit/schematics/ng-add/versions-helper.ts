ea23505ac420e4914af39b7aa3f8a47162c57770
Andrea Stagi <stagi.andrea@gmail.com>
2025-05-29 08:56:05 +0000

    feat: add schematics for angular 19

--- projects/design-angular-kit/schematics/ng-add/versions-helper.ts
+++ projects/design-angular-kit/schematics/ng-add/versions-helper.ts
@@ -1,18 +1,22 @@
 const ANGULAR_VERSIONS = {
   v17: '17',
   v18: '18',
+  v19: '19',
 };
 
 const KIT_VERSION_1_0_0 = '1.0.0';
-const KIT_VERSION_1_1_0 = '1.1.0';
+const KIT_VERSION_1_4_0 = '1.4.0';
+const KIT_VERSION_1_5_0 = '1.5.0';
+
 const DEFAULT = 'DEFAULT';
 const LATEST = 'LATEST';
 
 const versionsMap: Record<string, string> = {
   [DEFAULT]: KIT_VERSION_1_0_0,
   [ANGULAR_VERSIONS.v17]: KIT_VERSION_1_0_0,
-  [ANGULAR_VERSIONS.v18]: KIT_VERSION_1_1_0,
-  [LATEST]: KIT_VERSION_1_1_0,
+  [ANGULAR_VERSIONS.v18]: `~${KIT_VERSION_1_4_0}`,
+  [ANGULAR_VERSIONS.v19]: `~${KIT_VERSION_1_5_0}`,
+  [LATEST]: KIT_VERSION_1_5_0,
 };
 
 // Get Design Angular Kit package version given Angular major version
@@ -21,21 +25,9 @@ export function getPackageVersion({ angularMajorVersion }: { angularMajorVersion
     throw new Error('Major version is not a number');
   }
 
-  let version = versionsMap[DEFAULT];
-
-  switch (angularMajorVersion) {
-    case ANGULAR_VERSIONS.v17:
-      version = versionsMap[ANGULAR_VERSIONS.v17];
-      break;
-    case ANGULAR_VERSIONS.v18:
-      version = versionsMap[ANGULAR_VERSIONS.v18];
-      break;
-    default: {
-      if (Number(angularMajorVersion) > Number(ANGULAR_VERSIONS.v18)) {
-        version = versionsMap[LATEST];
-      }
-    }
+  if (Number(angularMajorVersion) > Number(ANGULAR_VERSIONS.v19)) {
+    return versionsMap[LATEST];
   }
 
-  return version;
+  return versionsMap[angularMajorVersion] ?? versionsMap[DEFAULT];
 }
