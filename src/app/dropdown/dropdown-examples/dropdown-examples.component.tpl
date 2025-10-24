6cf5aa1bc3c612ae7de55d4516b30f9dfbd756ae
Valerio <valeriocomo@users.noreply.github.com>
2025-10-24 15:21:37 +0200

    fix(a11y): generates the correct semantic structure for dropdown

--- src/app/dropdown/dropdown-examples/dropdown-examples.component.tpl
+++ src/app/dropdown/dropdown-examples/dropdown-examples.component.tpl
@@ -23,3 +23,15 @@
 <it-dropdown-link-example></it-dropdown-link-example>
 
 <it-source-display html="{$ sanitize(htmlLink) $}" typescript="{$ sanitize(typescriptLink) $}"></it-source-display>
+
+{% set htmlHostAttribute %}
+  {% include "../dropdown-host-attribute/dropdown-host-attribute.component.html" %}
+{% endset %}
+
+{% set typescriptHostAttribute %}
+  {% include "../dropdown-host-attribute/dropdown-host-attribute.component.ts" %}
+{% endset %}
+
+<it-dropdown-host-attribute></it-dropdown-host-attribute>
+
+<it-source-display html="{$ sanitize(htmlHostAttribute) $}" typescript="{$ sanitize(typescriptHostAttribute) $}"></it-source-display>
--- src/app/dropdown/dropdown.module.ts
+++ src/app/dropdown/dropdown.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { DropdownRoutingModule } from './dropdown-routing.module';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 import { DropdownExampleComponent } from './dropdown-example/dropdown-example.component';
 import { DropdownExamplesComponent } from './dropdown-examples/dropdown-examples.component';
+import { DropdownHostAttributeComponent } from './dropdown-host-attribute/dropdown-host-attribute.component';
 import { DropdownIndexComponent } from './dropdown-index/dropdown-index.component';
-import { SharedModule } from '../shared/shared.module';
-import { FormsModule } from '@angular/forms';
 import { DropdownLinkExampleComponent } from './dropdown-link-example/dropdown-link-example.component';
+import { DropdownRoutingModule } from './dropdown-routing.module';
 
 @NgModule({
   imports: [CommonModule, DropdownRoutingModule, SharedModule, FormsModule],
-  declarations: [DropdownExampleComponent, DropdownExamplesComponent, DropdownIndexComponent, DropdownLinkExampleComponent],
+  declarations: [
+    DropdownExampleComponent,
+    DropdownExamplesComponent,
+    DropdownIndexComponent,
+    DropdownLinkExampleComponent,
+    DropdownHostAttributeComponent,
+  ],
 })
 export class DropdownModule {}
