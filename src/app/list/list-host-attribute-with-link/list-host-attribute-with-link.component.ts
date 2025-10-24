1d52d98ce6bddbf5a8243f36fde0b25d75557b23
Valerio <valeriocomo@users.noreply.github.com>
2025-10-24 15:22:36 +0200

    fix(a11y): generates the correct semantic structure for lists

---
+++ src/app/list/list-host-attribute-with-link/list-host-attribute-with-link.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'it-list-host-attribute-with-link',
+  templateUrl: './list-host-attribute-with-link.component.html',
+  standalone: false,
+})
+export class ListHostAttributeWithLinkComponent {}
