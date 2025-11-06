5abc338a7149a9d6dd570add6e9830fa1f1a2df7
Andrea Stagi <stagi.andrea@gmail.com>
2025-11-06 16:53:25 +0100

    fix(a11y): generates the correct semantic structure for dropdown

---
+++ src/app/dropdown/dropdown-host-attribute/dropdown-host-attribute.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { ButtonColor, DropdownDirection } from 'design-angular-kit/interfaces/core';
+
+@Component({
+  selector: 'it-dropdown-host-attribute',
+  templateUrl: './dropdown-host-attribute.component.html',
+  standalone: false,
+})
+export class DropdownHostAttributeComponent {
+  color?: ButtonColor;
+  label = 'Click me';
+  isDark = false;
+  isFullWidth = false;
+  withHeading = true;
+
+  menuPlacement?: DropdownDirection;
+
+  items = [
+    {
+      link: 'https://www.google.com',
+      active: false,
+      disabled: false,
+      large: true,
+      icon: 'star-outline',
+      iconPosition: 'right',
+      text: 'Item 1',
+    },
+    {
+      link: 'https://www.google.com',
+      active: false,
+      disabled: true,
+      large: false,
+      icon: 'link',
+      iconPosition: 'right',
+      text: 'Item 2',
+    },
+    {
+      link: 'https://www.google.com',
+      active: true,
+      disabled: false,
+      large: true,
+      icon: undefined,
+      iconPosition: 'right',
+      text: 'Item 3',
+    },
+  ];
+
+  openTime = '';
+  closeTime = '';
+
+  onOpenEvent() {
+    this.openTime = new Date().toISOString();
+  }
+
+  onCloseEvent() {
+    this.closeTime = new Date().toISOString();
+  }
+}
