84fc952f7cecdda2cc8e5fac31af100549ffe581
Valerio Como <v.como@innovazione.gov.it>
2025-12-10 16:41:12 +0000

    fix: update import paths for SchematicTestRunner to remove unnecessary file extension

--- projects/design-angular-kit/schematics/ng-add/index.spec.ts
+++ projects/design-angular-kit/schematics/ng-add/index.spec.ts
@@ -1,4 +1,4 @@
-import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing/index';
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 import { Schema as SchematicOptions } from './schema';
 
